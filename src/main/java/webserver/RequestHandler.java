225ae4ce013e3cfe3481f61700c506f797e1f71b
zoouniak <jueun1025@naver.com>
2025-01-06 13:30:05 +0900

    feat : index.html 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,11 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,11 +21,21 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            // 요청 parser
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            String requests = bufferedReader.readLine();
+            String[] request = requests.split(" ");
+
+            String method = request[0];
+            String path = request[1];
+        
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            if (path.equals("/index.html")) {
+                byte[] body = Files.readAllBytes(new File("src/main/resources/static/main" + path).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos,body);
+                dos.flush();
+            }
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
