b67f3d6ce6e2797a8eb440831f2a60b069f8332a
GoGradually <chkun3109@gmail.com>
2025-01-06 21:15:11 +0900

    feat: HttpResponse 클래스 생성
    - HTTP 응답 데이터를 저장하는 책임 보유
    - send 메소드를 통해 응답을 수행하는 책임 보유

---
+++ src/main/java/webserver/HttpResponse.java
@@ -0,0 +1,74 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpResponse {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+    private String protocol;
+    private StatusCode statusCode;
+    private final Map<String, String> headers = new HashMap<>();
+    private final Map<String, String> cookies = new HashMap<>();
+    private byte[] body;
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public StatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(StatusCode statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getHeader(String key) {
+        return headers.get(key);
+    }
+
+    public void setHeader(String key, String value) {
+        headers.put(key, value);
+    }
+
+    public String getCookie(String key) {
+        return cookies.get(key);
+    }
+
+    public void setCookie(String key, String value) {
+        cookies.put(key, value);
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+        if (body != null) {
+            headers.put("Content-Length", String.valueOf(body.length));
+        }
+    }
+
+    public void send(DataOutputStream dos) throws IOException {
+        dos.writeBytes("HTTP/1.1 " + statusCode.code + " " + statusCode.message + "\n");
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            dos.writeBytes(header.getKey() + ": " + header.getValue() + "\n");
+        }
+        dos.writeBytes("\n");
+        if (body != null && body.length > 0) {
+            dos.write(body, 0, body.length);
+            dos.writeBytes("\n");
+        }
+        dos.flush();
+    }
+}
