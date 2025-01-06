29e5c231991376baf0b14fd3be77ddf97fd9ac03
songhyeonseong <shsum7@gmail.com>
2025-01-06 15:05:18 +0900

    feat : 웹서버 1단계 - 정적인 html 파일 응답
    - HTTP Request를 파싱하여 String[] 형태로 저장하는 readInputToArray 메서드 작성
    - 리소스 요청 경로를 추출하여 해당 경로의 파일을 가져온 뒤 byte[] 형태로 ResponseBody에 담는 로직 작성
    - Response Header에 적절한 Content-Type이 입력되도록 getContentType 메서드 작성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,17 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private final String STATIC_FILE_DIRECTORY_PATH = "src/main/resources/static";
 
     private Socket connection;
 
@@ -18,29 +19,87 @@ public class RequestHandler implements Runnable {
         this.connection = connectionSocket;
     }
 
-    public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        public void run() {
+            logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+                    connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+                DataOutputStream dos = new DataOutputStream(out);
+
+                String [] httpRequestHeader = readInputToArray(in);
+                String resourceName = httpRequestHeader[0].split(" ")[1];
+
+                File file = new File(STATIC_FILE_DIRECTORY_PATH, resourceName);
+
+                byte[] body = fileToByteArray(file);
+
+                response200Header(dos, body.length, resourceName);
+                responseBody(dos, body);
+
+            } catch (IOException e) {
+                logger.error(e.getMessage());
+            }
+        }
+
+    public static String[] readInputToArray(InputStream in) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        List<String> lines = new ArrayList<>();
+        String line;
+
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            lines.add(line);
+        }
+
+        return lines.toArray(new String[0]);
+    }
+
+    public static byte[] fileToByteArray(File file) {
+        byte[] fileBytes = new byte[(int) file.length()]; // 파일 크기만큼 배열 생성
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            int bytesRead = fis.read(fileBytes); // 파일 내용 읽기
+            if (bytesRead != fileBytes.length) {
+                throw new IOException("Could not completely read the file");
+            }
+            return fileBytes;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String resourceName) {
+        try {
+            String contentType = getContentType(resourceName);
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
+    private String getContentType(String resourceName) {
+        String extension = resourceName.substring(resourceName.lastIndexOf(".") + 1);
+
+        switch (extension) {
+            case "html":
+                return "text/html";
+            case "css":
+                return "text/css";
+            case "js":
+                return "text/javascript";
+            case "ico":
+                return "image/x-icon";
+            case "png":
+                return "image/png";
+            case "jpg":
+                return "image/jpeg";
+            case "svg":
+                return "image/svg+xml";
+            default:
+                return "text/plain";
         }
     }
 
