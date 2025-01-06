97c41ede597013801e3578c494b742675ddb3510
mikekks <thdalsrb79@naver.com>
2025-01-06 22:36:07 +0900

    feat: HttpMethod resolve 메서드 구현

--- src/main/java/http/HttpMethod.java
+++ src/main/java/http/HttpMethod.java
@@ -2,4 +2,16 @@ package http;
 
 public enum HttpMethod {
 	GET, POST, PUT, PATCH, DELETE, HEAD;
+
+	public static HttpMethod resolve(String method) {
+		return switch (method) {
+			case "GET" -> GET;
+			case "HEAD" -> HEAD;
+			case "POST" -> POST;
+			case "PUT" -> PUT;
+			case "PATCH" -> PATCH;
+			case "DELETE" -> DELETE;
+			default -> null;
+		};
+	}
 }
