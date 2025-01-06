7eacf8941bf3ab7badfdcecaa6496dcf46749894
admin <admin@adminui-MacBookPro.local>
2025-01-06 22:51:38 +0900

    feat: url 정보로 파일을 찾는 로직 변경 굳이 재귀를 통해서 접근을 할 필요 없이 파라미터로 받은 url 값을 BASE_DIRECTORY와 결합하여 파일 객체를 만들고 해당 파일이 존재하는지 확인하면 된다.

--- src/main/java/util/StaticFileProvider.java
+++ src/main/java/util/StaticFileProvider.java
@@ -13,18 +13,12 @@ public class StaticFileProvider {
     private static final String BASE_DIRECTORY = "./src/main/resources/static/";
 
     public static File findStaticFileByUrl(String url) {
-        File baseDir = new File(BASE_DIRECTORY);
-        File[] files = baseDir.listFiles();
+        String filePath = BASE_DIRECTORY + url;
 
-        if (files == null) {
-            return null;
-        }
+        File file = new File(filePath);
 
-        for (File file : files) {
-            File resultFile = findStaticFileByUrlInDirectory(file, baseDir + url);
-            if (resultFile != null) {
-                return resultFile;
-            }
+        if(file.exists()){
+            return file;
         }
 
         return null;
@@ -46,29 +40,6 @@ public class StaticFileProvider {
             logger.error(e.getMessage());
         }
 
-        return null;
-    }
-
-    private static File findStaticFileByUrlInDirectory(File file, String url) {
-        if(file.isDirectory()){
-            File[] subFiles = file.listFiles();
-
-            if(subFiles == null || subFiles.length == 0){
-                return null;
-            }
-
-            for (File subFile: subFiles){
-                File resultPath = findStaticFileByUrlInDirectory(subFile, url);
-
-                if(resultPath != null){
-                    return resultPath;
-                }
-            }
-        }
-
-        if (file.getPath().equals(url)) {
-            return file;
-        }
         return null;
     }
 }
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -25,9 +28,13 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                RequestHandler requestHandler = new RequestHandler(connection);
+
+                // 쓰레드 가져와서 실행
+                executorService.submit(requestHandler);
             }
+            // 쓰레드 풀 종료, 더 이상 새로운 쓰레드 요청을 받지 않고 기존의 쓰레드 동작이 끝난 후 종료한다.
+            executorService.shutdown();
         }
     }
 }
