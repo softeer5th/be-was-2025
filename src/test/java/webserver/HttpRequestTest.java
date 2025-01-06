30f4cc136ed5ea2b12cff5a9c8e955b1f318b3a7
GoGradually <chkun3109@gmail.com>
2025-01-06 14:49:01 +0900

    test: 쿼리 파라미터가 있는 HTTP 메시지 파싱 검증 테스트 작성

--- src/test/java/webserver/HttpRequestTest.java
+++ src/test/java/webserver/HttpRequestTest.java
@@ -32,4 +32,27 @@ class HttpRequestTest {
         assertThat(request.getHeader("Accept")).isEqualTo("text/html");
         assertThat(request.getProtocol()).isEqualTo("HTTP/1.1");
     }
+
+    @Test
+    @DisplayName("쿼리 파라미터가 있는 HTTP 메시지 파싱 검증 테스트")
+    void generateQueryParameterizedHttpRequest() throws IOException {
+        //given
+        BufferedReader reader = new BufferedReader(new StringReader("GET /path?param1=value1&param2=value2 " +
+                "HTTP/1.1\r\n" +
+                "Host: www.example.com\r\n" +
+                "User-Agent: Mozilla/5.0\r\n" +
+                "Accept: text/html\r\n" +
+                "\r\n"));
+        //when
+        HttpRequest request = new HttpRequest(reader);
+
+        //then
+        assertThat(request.getMethod()).isEqualTo(HttpMethod.GET);
+        assertThat(request.getHeader("Host")).isEqualTo("www.example.com");
+        assertThat(request.getHeader("User-Agent")).isEqualTo("Mozilla/5.0");
+        assertThat(request.getHeader("Accept")).isEqualTo("text/html");
+        assertThat(request.getProtocol()).isEqualTo("HTTP/1.1");
+        assertThat(request.getParameter("param1")).isEqualTo("value1");
+        assertThat(request.getParameter("param2")).isEqualTo("value2");
+    }
 }
