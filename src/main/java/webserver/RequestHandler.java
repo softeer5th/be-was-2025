db341107b776d0b356cb879690eb86fe3ab1bbcf
uri010 <dbfl0461@gmail.com>
2025-01-06 14:00:19 +0900

    [Feat] Request Header의 요청 url에 해당하는 정적인 html 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,15 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String BASE_DIRECTORY = "src/main/resources/static";
 
     private Socket connection;
 
@@ -23,9 +22,17 @@ public class RequestHandler implements Runnable {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
+
+            String requestLine = reader.readLine();
+            if (requestLine == null) return;
+
+            String[] tokens = requestLine.split(" ");
+            String url = tokens[1];
+            String filePath = BASE_DIRECTORY + url;
+            byte[] body = Files.readAllBytes(new File(filePath).toPath());
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
