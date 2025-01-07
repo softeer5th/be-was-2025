cb82b7a43c03291b761ab9a693736593e4194585
admin <admin@adminui-MacBookPro-2.local>
2025-01-07 12:19:53 +0900

    feat: 요청 파일의 확장자를 추출하고 MIME 타입으로 변경한다. content type 설정시 MIME 타입의 값으로 활용하여 설정한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 
+import model.Mime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.StaticFileProvider;
@@ -24,13 +25,13 @@ public class RequestHandler implements Runnable {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = null;
-            String url = null;
+            String path = null;
 
             String line = br.readLine();
             logger.debug("request line : {}", line);
 
-            if ((url = parseRequestPath(line)) != null) {
-                File resultFile = StaticFileProvider.findStaticFileByUrl(url);
+            if ((path = parseRequestPath(line)) != null) {
+                File resultFile = StaticFileProvider.findStaticFileByPath(path);
 
                 if (resultFile == null) {
                     throw new RuntimeException("해당 경로에 해당하는 파일이 없습니다.");
@@ -43,21 +44,35 @@ public class RequestHandler implements Runnable {
                 logger.debug("header:  {}", line);
             }
 
-            response200Header(dos, body.length);
+            String extension = extractFileExtension(path);
+
+            String mimeType = Mime.getMimeType(extension);
+
+            if(mimeType == null){
+                throw new RuntimeException("파일 확장자에 해당하는 MIME 타입이 없습니다.");
+            }
+
+            response200Header(dos, mimeType, body.length);
             responseBody(dos, body);
         } catch (IOException | RuntimeException e) {
             logger.error(e.getMessage());
         }
     }
+
     private String parseRequestPath(String requestFirstLine){
         String[] requestParts = requestFirstLine.split(" ");
         return requestParts[1];
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private String extractFileExtension(String path){
+        String[] pathParts = path.split("\\.");
+        return pathParts[pathParts.length - 1];
+    }
+
+    private void response200Header(DataOutputStream dos, String mimeType, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes(String.format("Content-Type: %s;charset=utf-8\r\n", mimeType));
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
