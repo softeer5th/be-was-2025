daf07d013cad087fb51325e2f42887389a02afb2
GoGradually <chkun3109@gmail.com>
2025-01-07 15:53:53 +0900

    feat: 예외 페이지 서블릿 생성
    - File Not Found - 존재하지 않는 리소스에 접근했을 시 보여주는 페이지
    - File Not Supported - 지원하지 않는 확장자를 요청했을 시 보여주는 페이지

---
+++ src/main/java/webserver/servlet/FileNotSupportedErrorPageServlet.java
@@ -0,0 +1,14 @@
+package webserver.servlet;
+
+import webserver.httpserver.HttpRequest;
+import webserver.httpserver.HttpResponse;
+import webserver.httpserver.StatusCode;
+
+public class FileNotSupportedErrorPageServlet implements Servlet {
+    @Override
+    public void handle(HttpRequest request, HttpResponse response) {
+        response.setStatusCode(StatusCode.NOT_SUPPORTED);
+        response.setHeader("Content-Type", "text/html");
+        response.setBody("<h1> 406 Not Supported </h1>".getBytes());
+    }
+}
