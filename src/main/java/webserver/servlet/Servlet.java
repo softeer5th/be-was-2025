b9b61ecd77820f1be4f011592973359de4057464
GoGradually <chkun3109@gmail.com>
2025-01-07 13:31:49 +0900

    feat: Servlet의 handler 메소드가 IOException 을 던지도록 변경

--- src/main/java/webserver/servlet/Servlet.java
+++ src/main/java/webserver/servlet/Servlet.java
@@ -3,6 +3,8 @@ package webserver.servlet;
 import webserver.httpserver.HttpRequest;
 import webserver.httpserver.HttpResponse;
 
+import java.io.IOException;
+
 public interface Servlet {
-    void handle(HttpRequest request, HttpResponse response);
+    void handle(HttpRequest request, HttpResponse response) throws IOException;
 }
