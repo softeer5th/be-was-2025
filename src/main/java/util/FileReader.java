7f96e17d897ea93baede18143872ca80b9f184ce
zoouniak <jueun1025@naver.com>
2025-01-07 10:44:55 +0900

    refactor : Optional 반환으로 변경

--- src/main/java/util/FileReader.java
+++ src/main/java/util/FileReader.java
@@ -6,16 +6,16 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Optional;
 
 public abstract class FileReader {
     private static final Logger logger = LoggerFactory.getLogger(FileReader.class);
 
-    public static byte[] readFile(String fileName) {
+    public static Optional<byte[]> readFile(String fileName) {
         try {
-            return Files.readAllBytes(new File(fileName).toPath());
+            return Optional.of(Files.readAllBytes(new File(fileName).toPath()));
         } catch (IOException e) {
-            logger.error(e.getMessage());
-            return null;
+            return Optional.empty();
         }
     }
 }
