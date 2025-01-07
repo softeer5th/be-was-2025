dd25b42d7b78676e587809316758e93c3e28ff6b
GoGradually <chkun3109@gmail.com>
2025-01-07 18:59:10 +0900

    feat: Bad Request 서블릿 생성
    - 파싱 시 문제가 발생할 경우나 HTTP 메시지가 잘못되었을 경우 400 Bad Request 페이지 응답

--- src/main/java/servlet/ServletManager.java
+++ src/main/java/servlet/ServletManager.java
@@ -12,11 +12,13 @@ import java.util.Map;
 public class ServletManager {
     private static final String NOT_FOUND = "NOT_FOUND";
     private static final String FILE_NOT_SUPPORTED = "FILE_NOT_SUPPORTED";
+    private static final String BAD_REQUEST = "BAD_REQUEST";
     private final Map<String, Servlet> servlets = new HashMap<>();
     public ServletManager() {
         servlets.put("default", new StaticResourceServlet());
         servlets.put(FILE_NOT_SUPPORTED, new FileNotSupportedErrorPageServlet());
         servlets.put(NOT_FOUND, new FileNotFoundPageServlet());
+        servlets.put(BAD_REQUEST, new BadRequestServlet());
     }
 
     public void addServlet(String url, Servlet servlet) {
@@ -35,6 +37,8 @@ public class ServletManager {
             servlets.get(FILE_NOT_SUPPORTED).handle(request, response);
         } catch (FileNotFoundException e){
             servlets.get(NOT_FOUND).handle(request, response);
+        } catch (IOException | IllegalArgumentException e) {
+            servlets.get(BAD_REQUEST).handle(request, response);
         }
     }
 }
