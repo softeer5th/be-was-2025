094628c31359859360e7042d3207bb87477c9dc5
codrin2 <hssang127@gmail.com>
2025-01-07 15:01:41 +0900

    feat(ContentTypeMapper): 확장자와 Content-Type을 관리하는 Mapper 구현

---
+++ src/main/java/webserver/ContentTypeMapper.java
@@ -0,0 +1,27 @@
+package webserver;
+
+import java.util.Map;
+
+public class ContentTypeMapper {
+    private static final Map<String, String> MIME_TYPES = Map.of(
+            "html", "text/html",
+            "css", "text/css",
+            "js", "application/javascript",
+            "ico", "image/x-icon",
+            "png", "image/png",
+            "jpg", "image/jpeg"
+    );
+
+    public static String getContentType(String path) {
+        String extension = getExtension(path);
+        return MIME_TYPES.getOrDefault(extension, "application/octet-stream");
+    }
+
+    private static String getExtension(String path) {
+        int lastDot = path.lastIndexOf('.');
+        if (lastDot == -1) {
+            return "";
+        }
+        return path.substring(lastDot + 1).toLowerCase();
+    }
+}
