8f68236a5f99a346c2fdbdcd53694359da67cd56
codrin2 <hssang127@gmail.com>
2025-01-07 15:15:34 +0900

    refactor(WebServer): ThreadPoolExecutor를 사용해 쓰레드 풀 세부 설정 가능하도록 변경

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,8 +2,7 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +10,12 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int CORE_POOL_SIZE = 10;
+    private static final int MAX_POOL_SIZE = 20;
+    private static final long KEEP_ALIVE_TIME = 60L;
+    private static final int QUEUE_CAPACITY = 50;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         int port;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
@@ -20,7 +23,14 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ExecutorService executorService = new ThreadPoolExecutor(
+                CORE_POOL_SIZE,
+                MAX_POOL_SIZE,
+                KEEP_ALIVE_TIME,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(QUEUE_CAPACITY),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
 
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
