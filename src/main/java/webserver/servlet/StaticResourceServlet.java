288206d9567cbf338124f6dd278e85a48c7956c8
GoGradually <chkun3109@gmail.com>
2025-01-07 13:29:32 +0900

    feat: 정적 리소스 서블릿 구현

--- src/main/java/webserver/servlet/StaticResourceServlet.java
+++ src/main/java/webserver/servlet/StaticResourceServlet.java
@@ -2,10 +2,30 @@ package webserver.servlet;
 
 import webserver.httpserver.HttpRequest;
 import webserver.httpserver.HttpResponse;
+import webserver.httpserver.StatusCode;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class StaticResourceServlet implements Servlet {
     @Override
-    public void handle(HttpRequest request, HttpResponse response) {
+    public void handle(HttpRequest request, HttpResponse response) throws IOException {
 
+        response.setProtocol(request.getProtocol());
+        response.setStatusCode(StatusCode.OK);
+        response.setHeader("Content-Type", request.guessContentType());
+
+        File file = new File("src/main/resources/static" + request.getUri());
+        byte[] readFile = getFile(file);
+        response.setBody(readFile);
+
+    }
+    private byte[] getFile(File file) throws IOException {
+        try(FileInputStream fis = new FileInputStream(file)) {
+            byte[] bytes = new byte[(int) file.length()];
+            fis.read(bytes);
+            return bytes;
+        }
     }
 }
