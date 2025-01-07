792d0d8df77a992cd9b5957d63b3885512075eab
GoGradually <chkun3109@gmail.com>
2025-01-07 13:29:13 +0900

    feat: 서블릿 매니저 구현
    - default 서블릿: 동적 매핑된 리소스가 없을 경우, 정적 데이터를 서빙하는 서블릿
    - error 서블릿: 서블릿이 handle 도중 예외가 발생했을 시, 에러 페이지를 서빙하는 서블릿
    - IOException 발생 시 밖으로 예외 던짐

--- src/main/java/webserver/servlet/ServletManager.java
+++ src/main/java/webserver/servlet/ServletManager.java
@@ -1,8 +1,33 @@
 package webserver.servlet;
 
+import webserver.httpserver.HttpRequest;
+import webserver.httpserver.HttpResponse;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServletManager {
     private final Map<String, Servlet> servlets = new HashMap<>();
+
+    public ServletManager() {
+        servlets.put("default", new StaticResourceServlet());
+        servlets.put("error", new ErrorPageServlet());
+    }
+
+    public void addServlet(String url, Servlet servlet) {
+        servlets.put(url, servlet);
+    }
+
+    public void serve(HttpRequest request, HttpResponse response) throws IOException {
+        String uri = request.getUri();
+        try {
+            if (!servlets.containsKey(uri)) {
+                servlets.get("default").handle(request, response);
+            }
+            servlets.get(uri).handle(request, response);
+        } catch (FileNotSupportedException e) {
+            servlets.get("error").handle(request, response);
+        }
+    }
 }
