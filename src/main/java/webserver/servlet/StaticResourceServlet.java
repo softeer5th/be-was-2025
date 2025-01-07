d20600913af60e34800be71f5f1f4b63fe5b0b0b
GoGradually <chkun3109@gmail.com>
2025-01-07 15:41:23 +0900

    refactor: 프로토콜이 중복 등록되던 문제 수정

--- src/main/java/webserver/servlet/StaticResourceServlet.java
+++ src/main/java/webserver/servlet/StaticResourceServlet.java
@@ -13,14 +13,11 @@ import static utils.FileUtils.getFile;
 public class StaticResourceServlet implements Servlet {
     @Override
     public void handle(HttpRequest request, HttpResponse response) throws IOException {
-
-        response.setProtocol(request.getProtocol());
         response.setStatusCode(StatusCode.OK);
         response.setHeader("Content-Type", request.guessContentType());
 
         File file = new File("src/main/resources/static" + request.getUri());
         byte[] readFile = getFile(file);
         response.setBody(readFile);
-
     }
 }
