b55b3ba1a4619caf41e3227f85a713425c796173
GoGradually <chkun3109@gmail.com>
2025-01-07 13:30:24 +0900

    feat: 서블릿 매니저 적용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -8,6 +9,7 @@ import java.util.concurrent.Executors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.httpserver.RequestHandler;
+import webserver.servlet.ServletManager;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
@@ -21,6 +23,7 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
         ExecutorService executor = Executors.newFixedThreadPool(200);
+        ServletManager servletManager = new ServletManager();
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -28,8 +31,10 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                executor.submit(new RequestHandler(connection));
+                executor.submit(new RequestHandler(connection, servletManager));
             }
+        } catch (IOException e){
+            logger.error(e.getMessage(), e);
         }
     }
 }
