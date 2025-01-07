3c8cdd1ea9edc93b0c790e79b779a5daa1995d7c
scc9811 <wjsrudtjr98@naver.com>
2025-01-07 16:20:31 +0900

    style : 오타 수정
    POLL -> POOL

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -12,7 +12,7 @@ public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
     // Size 설정 기준 : 8 (CPU 개수) * 0.6 (각 코어의 목표 사용률) + 0 (I/O 등 블로킹 호출 개수)
-    private static final int THREAD_POLL_SIZE = 5;
+    private static final int THREAD_POOL_SIZE = 5;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -22,7 +22,7 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POLL_SIZE);
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
