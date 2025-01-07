490710cd0b3a2806ca2a32d1d40e008140e885b1
GoGradually <chkun3109@gmail.com>
2025-01-07 15:43:37 +0900

    feat: 홈 서블릿 생성
    - 홈의 index.html을 서빙하도록 설정

--- src/main/java/webserver/servlet/HomeServlet.java
+++ src/main/java/webserver/servlet/HomeServlet.java
@@ -2,10 +2,21 @@ package webserver.servlet;
 
 import webserver.httpserver.HttpRequest;
 import webserver.httpserver.HttpResponse;
+import webserver.httpserver.StatusCode;
+
+import java.io.File;
+import java.io.IOException;
+
+import static utils.FileUtils.getFile;
 
 public class HomeServlet implements Servlet {
     @Override
-    public void handle(HttpRequest request, HttpResponse response) {
+    public void handle(HttpRequest request, HttpResponse response) throws IOException {
+        response.setStatusCode(StatusCode.OK);
+        response.setHeader("Content-Type", request.guessContentType());
 
+        File file = new File("src/main/resources/static/index.html");
+        byte[] readFile = getFile(file);
+        response.setBody(readFile);
     }
 }
