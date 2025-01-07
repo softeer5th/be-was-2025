4adaf0874e3c6b1eb7f80cf51fbe8a4c4cbcd475
성필 <philip000412@gmail.com>
2025-01-07 10:35:33 +0900

    feat: 디렉토리 판단, 파일 Content-Type 추론 기능 추가

--- src/main/java/util/HttpRequestUtil.java
+++ src/main/java/util/HttpRequestUtil.java
@@ -1,6 +1,20 @@
 package util;
 
+import java.util.Map;
+
 public class HttpRequestUtil {
+
+    private static Map<String, String> contentType = Map.of(
+            "html", "text/html",
+            "css", "text/css",
+            "js", "text/javascript",
+            "ico", "image/x-icon",
+            "png", "image/png",
+            "jpg", "image/jpeg",
+            "jpeg", "image/jpeg",
+            "svg", "image/svg+xml"
+    );
+
     public static String getUrl(String inputString) {
         if (inputString == null) {
             return null;
@@ -12,5 +26,19 @@ public class HttpRequestUtil {
             return null;
         }
         return tokens[1];
+    }
+
+    public static boolean isDirectory(String inputString) {
+        return inputString != null && !inputString.contains(".");
+    }
+
+    public static String getType(String inputString) {
+        String type = inputString.split("\\.")[2];
+        String typeString = contentType.get(type);
+        if (typeString == null) {
+            typeString = "text/html";
+        }
+
+        return typeString;
     }
 }
