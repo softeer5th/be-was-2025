2de18f5826822b54b9e3116bcb45462a528d4489
GoGradually <chkun3109@gmail.com>
2025-01-07 15:56:57 +0900

    feat: HTTP 응답코드 구현
    - 2XX~4XX까지 구현

--- src/main/java/webserver/httpserver/StatusCode.java
+++ src/main/java/webserver/httpserver/StatusCode.java
@@ -1,7 +1,21 @@
 package webserver.httpserver;
 
 public enum StatusCode {
-    OK(200, "OK"), NOT_FOUND(404, "Not Found");
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    SEE_OTHER(303, "See Other"),
+    NOT_MODIFIED(304, "Not Modified"),
+    TEMPORARY_REDIRECT(307, "Temporary Redirect"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    PAYMENT_REQUIRED(402, "Payment Required"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    NOT_SUPPORTED(406, "Not Supported");
 
     int code;
     String message;
