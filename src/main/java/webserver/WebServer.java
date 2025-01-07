60dc5f489d6642f08a8e2937aed5b07aa7b20044
GoGradually <chkun3109@gmail.com>
2025-01-07 15:46:54 +0900

    feat: 웹 서버 생성 시, 서블릿 등록
    - "/" 을 홈으로 이용
    - "/registration" 을 회원가입 페이지로 이용
    - "/create" 를 회원가입 post 메소드를 가장한 get 으로 이용
    - "/success" 를 PRG 를 위한 회원가입 성공 페이지로 이용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -9,7 +9,7 @@ import java.util.concurrent.Executors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.httpserver.RequestHandler;
-import webserver.servlet.ServletManager;
+import webserver.servlet.*;
 
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
@@ -23,7 +23,13 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
         ExecutorService executor = Executors.newFixedThreadPool(200);
+
         ServletManager servletManager = new ServletManager();
+        servletManager.addServlet("/", new HomeServlet());
+        servletManager.addServlet("/registration", new SingUpServlet());
+        servletManager.addServlet("/create", new CreateServlet());
+        servletManager.addServlet("/success", new SignUpSuccessServlet());
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
