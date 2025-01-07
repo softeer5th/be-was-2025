98369c7f851867f6f5db3e7ecead9293c5402948
GoGradually <chkun3109@gmail.com>
2025-01-07 16:08:15 +0900

    feat: 회원 생성 서블릿 추가
    - PRG 적용

--- src/main/java/webserver/servlet/CreateServlet.java
+++ src/main/java/webserver/servlet/CreateServlet.java
@@ -1,11 +1,34 @@
 package webserver.servlet;
 
+import db.Database;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.httpserver.HttpRequest;
 import webserver.httpserver.HttpResponse;
+import webserver.httpserver.StatusCode;
+
+import java.io.File;
+import java.io.IOException;
+
+import static db.Database.addUser;
+import static utils.FileUtils.getFile;
 
 public class CreateServlet implements Servlet {
-    @Override
-    public void handle(HttpRequest request, HttpResponse response) {
 
+    private static final Logger log = LoggerFactory.getLogger(CreateServlet.class);
+
+    @Override
+    public void handle(HttpRequest request, HttpResponse response) throws IOException {
+        String userId = request.getParameter("userId");
+        String password = request.getParameter("password");
+        String name = request.getParameter("name");
+        String email = null;
+        User user = new User(userId, password, name, email);
+        addUser(user);
+
+        log.info("User created: " + user);
+        response.setStatusCode(StatusCode.SEE_OTHER);
+        response.setHeader("Location", "/success");
     }
 }
