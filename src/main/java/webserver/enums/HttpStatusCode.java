41059bea30c9980bfa6eda10da813c3c309b10c1
vvsos1 <vvsos10@naver.com>
2025-01-07 09:58:23 +0900

    feat. add http enum
    http 관련 enum들을 추가

---
+++ src/main/java/webserver/enums/HttpStatusCode.java
@@ -0,0 +1,34 @@
+package webserver.enums;
+
+public enum HttpStatusCode {
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    NO_CONTENT(204, "No Content"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    HTTP_VERSION_NOT_SUPPORTED(505, "HTTP Version Not Supported");
+
+    public final Integer statusCode;
+    public final String message;
+
+    HttpStatusCode(int code, String message) {
+        this.statusCode = code;
+        this.message = message;
+    }
+
+    public static HttpStatusCode of(int statusCode) {
+        for (HttpStatusCode httpStatusCode : HttpStatusCode.values()) {
+            if (httpStatusCode.statusCode == statusCode) {
+                return httpStatusCode;
+            }
+        }
+        throw new IllegalArgumentException("존재하지 않는 상태 코드입니다.");
+    }
+
+}
