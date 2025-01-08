79922cb1958c4b82be0231c980a2e86e20572d70
admin <admin@adminui-MacBookPro-2.local>
2025-01-08 16:09:21 +0900

    feat: 해당 파일 객체가 디렉토리라면 해당 디렉토리의 index.html 파일에 접근한다.

--- src/main/java/util/FileUtil.java
+++ src/main/java/util/FileUtil.java
@@ -10,22 +10,28 @@ import java.io.IOException;
 
 public class FileUtil {
     private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
-    private static final String BASE_DIRECTORY = "./src/main/resources/static/";
+    private static final String BASE_DIRECTORY = "./src/main/resources/static";
 
-    public static File findStaticFileByPath(String path) {
+    public static File findFileByPath(String path) {
         String filePath = BASE_DIRECTORY + path;
 
         File file = new File(filePath);
 
+        // 해당 파일 객체가 디렉토리라면 해당 디렉토리의 index.html 파일을 보여준다.
+        if(file.isDirectory()){
+            filePath += "/index.html";
+            return new File(filePath);
+        }
+
         // 파일 객체가 존재하는 지만 확인하는 것이 아닌 해당 파일 객체가 디렉토리인지 확인하는 과정이 필요하다.
-        if(file.exists() && !file.isDirectory()){
+        if(file.exists()){
             return file;
         }
 
         return null;
     }
 
-    public static byte[] readStaticFileToByteArray(File file){
+    public static byte[] readFileToByteArray(File file){
         try (FileInputStream fis = new FileInputStream(file);
              ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ) {
