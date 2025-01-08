04b83fb54092feb590b41ab3a5d97eec8c80b2d7
zoouniak <jueun1025@naver.com>
2025-01-08 15:58:47 +0900

    refactor : 존재하지 않는 파일 요청할 경우 404 처리

--- src/main/java/webserver/StaticFileHandler.java
+++ src/main/java/webserver/StaticFileHandler.java
@@ -9,7 +9,6 @@ import util.RequestInfo;
 
 import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 /*
  *  정적 파일 요청을 담당하는  Handler
@@ -18,7 +17,7 @@ public class StaticFileHandler implements Handler {
     private static final String STATIC_FILE_PATH = "src/main/resources/static";
 
     @Override
-    public void handle(RequestInfo request, DataOutputStream dataOutputStream) throws IOException {
+    public void handle(RequestInfo request, DataOutputStream dataOutputStream) {
         String path = request.getPath();
 
         FileContentType extension = FileContentType.getExtensionFromPath(path);
@@ -28,10 +27,17 @@ public class StaticFileHandler implements Handler {
         response.setStatus(HttpStatus.OK);
         response.setContentType(extension);
 
-        byte[] body = FileReader.readFile(STATIC_FILE_PATH + path)
-                .orElseThrow(() -> new FileNotFoundException(path));
-        response.setBody(body);
+        try {
+            byte[] body = FileReader.readFile(STATIC_FILE_PATH + path)
+                    .orElseThrow(() -> new FileNotFoundException(path));
+            response.setBody(body);
 
-        response.send(dataOutputStream);
+            response.send(dataOutputStream);
+        } catch (FileNotFoundException e) {
+            response.setStatus(HttpStatus.NOT_FOUND);
+            response.setContentType(FileContentType.HTML);
+            response.setBody("file not found");
+            response.send(dataOutputStream);
+        }
     }
 }
