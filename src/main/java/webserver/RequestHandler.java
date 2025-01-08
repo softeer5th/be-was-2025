fbaa16554441849c7875f73a70e208a9f068c0c7
zoouniak <jueun1025@naver.com>
2025-01-08 14:33:36 +0900

    refactor : 요청을 적절한 핸들러가 처리하도록 변경

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,20 +2,31 @@ package webserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.*;
-import util.FileReader;
+import util.RequestInfo;
+import util.RequestParser;
 
-import java.io.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-    private static final String STATIC_FILE_PATH = "src/main/resources/static";
-
     private Socket connection;
+
+    private final Map<Pattern, Handler> routeMap;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
+        routeMap = new HashMap<>();
+        routeMap.put(Pattern.compile("^/user/.+$"), new UserRequestHandler());
+        routeMap.put(Pattern.compile("^.*\\.(html|css|js|svg)$"), new StaticFileHandler());
     }
 
     public void run() {
@@ -24,25 +35,24 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             RequestInfo requestInfo = RequestParser.parse(in);
-
-            HttpMethod method = requestInfo.getMethod();
             String path = requestInfo.getPath();
 
-            FileContentType extension = FileContentType.getExtensionFromPath(path);
-
-            HttpResponse response = new HttpResponse();
-
             DataOutputStream dos = new DataOutputStream(out);
-            response.setStatus(HttpStatus.OK);
-            response.setContentType(extension);
+            Handler handler = getHandler(path)
+                    .orElseThrow(() -> new UnsupportedOperationException("No handler found for path" + path));
 
-            byte[] body = FileReader.readFile(STATIC_FILE_PATH + path)
-                    .orElseThrow(() -> new FileNotFoundException(path));
-            response.setBody(body);
-
-            response.send(dos);
+            handler.handle(requestInfo, dos);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private Optional<Handler> getHandler(String path) {
+        for (Pattern pattern : routeMap.keySet()) {
+            Matcher matcher = pattern.matcher(path);
+            if (matcher.matches())
+                return Optional.of(routeMap.get(pattern));
+        }
+        return Optional.empty();
     }
 }
