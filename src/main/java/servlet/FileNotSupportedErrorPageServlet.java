1bf9df56c1d9212c3e207fd0c8472d80954e6c22
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-08 15:26:38 +0900

    feat: 지원하지 않는 MIME 타입의 경우, 406 Not Acceptable 과 응답 가능한 컨텐츠 타입을 알리도록 변경

--- src/main/java/servlet/FileNotSupportedErrorPageServlet.java
+++ src/main/java/servlet/FileNotSupportedErrorPageServlet.java
@@ -4,11 +4,19 @@ import webserver.httpserver.HttpRequest;
 import webserver.httpserver.HttpResponse;
 import webserver.httpserver.StatusCode;
 
+import java.io.File;
+import java.io.IOException;
+
+import static utils.FileUtils.getFile;
+
 public class FileNotSupportedErrorPageServlet implements Servlet {
     @Override
-    public void handle(HttpRequest request, HttpResponse response) {
+    public void handle(HttpRequest request, HttpResponse response) throws IOException {
         response.setStatusCode(StatusCode.NOT_SUPPORTED);
         response.setHeader("Content-Type", "text/html");
         response.setBody("<h1> 406 Not Supported </h1>".getBytes());
+        File file = new File("src/main/resources/static/error/notAcceptable.html");
+        byte[] readFile = getFile(file);
+        response.setBody(readFile);
     }
 }
