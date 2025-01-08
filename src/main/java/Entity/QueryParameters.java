c128281e3ee6bfc8a76e656e9cb46e24af56ee69
scc9811 <wjsrudtjr98@naver.com>
2025-01-08 15:35:12 +0900

    feat : url의 쿼리 파라미터 파싱 기능 추가

---
+++ src/main/java/Entity/QueryParameters.java
@@ -0,0 +1,30 @@
+package Entity;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class QueryParameters {
+    Map<String, String> map;
+
+    public QueryParameters(String s) {
+        map = new HashMap<>();
+        String[] pairs = s.split("&");
+        for (String pair : pairs) {
+            String[] tokens = pair.split("=");
+            add(tokens[0], tokens[1]);
+        }
+    }
+
+    public String get(String key) {
+        return map.get(key);
+    }
+
+    public Set<String> getKeySet() {
+        return map.keySet();
+    }
+
+    private void add(String key, String value) {
+        map.put(key, value);
+    }
+}
