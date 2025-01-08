4bf07b4c6b262026d70cfa8e6a318c8ff538e57f
mikekks <thdalsrb79@naver.com>
2025-01-08 16:19:47 +0900

    refactor: HttpRequest 표준에 맞게 객체지향적으로 설계

---
+++ src/main/java/http/request/HttpHeaders.java
@@ -0,0 +1,62 @@
+package http.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HttpHeaders {
+	private static final String CRLF = "\r\n";
+	private final String HEADER_DELIMITER = ": ";
+	private final String HEADER_VALUE_DELIMITER = ",";
+	private Map<String, List<String>> headers = new HashMap<>();
+
+	public HttpHeaders(BufferedReader reader) throws IOException {
+		String line;
+		while ((line = reader.readLine()) != null && !line.isEmpty()) {
+			String[] headerParts = line.split(HEADER_DELIMITER, 2);
+			String headerName = headerParts[0].strip().toLowerCase();
+
+			String headerValue = (headerParts.length > 1) ? headerParts[1].strip() : "";
+			String[] values = headerValue.split(HEADER_VALUE_DELIMITER);
+
+			List<String> valueList = new ArrayList<>();
+			for (String value : values) {
+				valueList.add(value.strip());
+			}
+
+			headers.put(headerName, valueList);
+		}
+	}
+
+	public HttpHeaders() {
+	}
+
+	public boolean containsHeader(String headerName){
+		return headers.containsKey(headerName);
+	}
+
+	public List<String> getHeader(String name) {
+		return headers.get(name.toLowerCase());
+	}
+
+	public void setHeader(String headerName, String... values) {
+		List<String> valueList = new ArrayList<>();
+		for (String value : values) {
+			valueList.add(value.strip());
+		}
+		headers.put(headerName, valueList);
+	}
+
+	public Map<String, List<String>> getHeaders() {
+		return headers;
+	}
+
+	public String getHeaderToString(String headerName){
+		String headerValue = String.join(HEADER_VALUE_DELIMITER, headers.get(headerName));
+
+		return headerName + HEADER_DELIMITER + headerValue + CRLF;
+	}
+}
---
+++ src/main/java/http/request/HttpRequestLine.java
@@ -0,0 +1,51 @@
+package http.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import enums.ContentType;
+import enums.HttpMethod;
+
+public class HttpRequestLine {
+	private static final String REQUEST_LINE_DELIMITER = " ";
+	private static final int REQUEST_LINE_LENGTH = 3;
+
+	private HttpMethod method;
+	private String uri;
+	private String version;
+
+	public HttpRequestLine(BufferedReader reader) throws IOException {
+		String requestLine = reader.readLine();
+
+		if (requestLine == null || requestLine.isEmpty()) {
+			throw new IllegalArgumentException("Empty request line");
+		}
+
+		String[] parts = requestLine.split(REQUEST_LINE_DELIMITER);
+
+		if (parts.length != REQUEST_LINE_LENGTH) {
+			throw new IllegalArgumentException("Malformed request line: " + requestLine);
+		}
+
+		this.method = HttpMethod.resolve(parts[0]);
+		this.uri = parts[1];
+		this.version = parts[2];
+	}
+
+	public HttpMethod getMethod() {
+		return method;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public ContentType inferContentType(){
+		String extention = uri.substring(uri.lastIndexOf(".") + 1);
+		return ContentType.from(extention);
+	}
+}
