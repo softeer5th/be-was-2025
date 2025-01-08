ef55b5b1cdd38bea4cced2b5f9406d719fd8da6f
codrin2 <hssang127@gmail.com>
2025-01-08 16:36:40 +0900

    feat(RequestHandler): ApiRouter 적용하여 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 
+import api.ApiRouter;
 import model.RequestData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,7 @@ public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private final StaticResourceLoader resourceLoader = new StaticResourceLoader("src/main/resources/static");
     private final HttpRequestParser requestParser = new HttpRequestParser();
+    private final ApiRouter apiRouter = new ApiRouter();
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -26,15 +28,29 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             RequestData requestData = requestParser.parse(in);
-            LoadResult responseLoadResult = resourceLoader.load(requestData.path());
+
+            LoadResult apiResult = apiRouter.route(requestData);
 
             HttpResponse response = new HttpResponse(new DataOutputStream(out));
-            if (responseLoadResult.content() == null) {
+
+            if (apiResult != null) {
+                if (apiResult.content() == null) {
+                    byte[] notFound = "<h1>400 Bad Request</h1>".getBytes();
+                    response.send404(notFound);
+                    return;
+                }
+                response.send200(apiResult.content(), apiResult.path());
+                return;
+            }
+
+            LoadResult resourceResult = resourceLoader.load(requestData.path());
+            if (resourceResult.content() == null) {
                 byte[] notFoundBody = "<h1>404 File Not Found</h1>".getBytes();
                 response.send404(notFoundBody);
-            } else {
-                response.send200(responseLoadResult.content(), responseLoadResult.path());
+                return;
             }
+
+            response.send200(resourceResult.content(), resourceResult.path());
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
