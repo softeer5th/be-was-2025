74964834be27708a074a5844663275a972d54cae
zoouniak <jueun1025@naver.com>
2025-01-08 16:18:04 +0900

    refactor : setter 사용

--- src/main/java/webserver/UserRequestHandler.java
+++ src/main/java/webserver/UserRequestHandler.java
@@ -16,19 +16,21 @@ public class UserRequestHandler implements Handler {
     private static final Logger logger = LoggerFactory.getLogger(UserRequestHandler.class);
 
     @Override
-    public HttpResponse handle(RequestInfo request)  {
+    public HttpResponse handle(RequestInfo request) {
         logger.debug("request : {} ", request);
 
         String path = request.getPath().substring("/user/".length());
         HttpResponse response = new HttpResponse();
 
         if (request.getMethod().equals(GET) && path.startsWith("create?")) {
-            UserCreateRequest userCreateRequest = UserCreateRequest.of(path.substring("create?".length()));
-            createUser(userCreateRequest);
+            try {
+                UserCreateRequest userCreateRequest = UserCreateRequest.of(path.substring("create?".length()));
+                createUser(userCreateRequest);
 
-            response.setStatus(HttpStatus.CREATED);
-            response.setContentType(FileContentType.HTML);
-            response.setBody("Successfully created user");
+                response.setResponse(HttpStatus.CREATED, FileContentType.HTML, "successfully created.!");
+            } catch (IllegalArgumentException e) {
+                response.setResponse(HttpStatus.BAD_REQUEST, FileContentType.HTML, e.getMessage());
+            }
         }
 
         return response;
