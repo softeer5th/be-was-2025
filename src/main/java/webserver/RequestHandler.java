934e7eb42e37bce38a82828fc5735f40516e0c8b
keemjoonsung <kjs990114@naver.com>
2025-01-09 17:36:00 +0900

    [feat: RequestHandler에서 동적 페이지 요청 처리 구현] 실행시 Servlet Mapper를 초기화하고, servlet.xml에서 읽어들여 map으로 관리하도록 함 Java Reflection을 이용하여 해당 서블릿 클래스를 동적으로 생성하고 service 메소드를 실행하여 서블릿 처리 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,6 +1,8 @@
 package webserver;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.Socket;
 
 import org.slf4j.Logger;
@@ -9,12 +11,17 @@ import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 import webserver.http.HttpStatus;
 import webserver.http.MimeType;
+import webserver.http.servlet.HttpServlet;
+import webserver.http.servlet.ServletInfo;
+import webserver.http.servlet.ServletMapper;
+import webserver.support.HttpRequestParser;
 import webserver.support.ResourceResolver;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
     private Socket connection;
+
+    private final ServletMapper servletManager = new ServletMapper();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -26,19 +33,23 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             DataOutputStream dos = new DataOutputStream(out);
-            HttpRequest request = new HttpRequest(in);
+
+            HttpRequest request = HttpRequestParser.parse(in);
             HttpResponse response = new HttpResponse(request, dos);
-            logger.debug("Request: {}", request);
+
             if (requestStaticResource(request)) {
                 serveStaticResource(request, response);
+            } else {
+                handleServlet(request,response);
             }
-        } catch (IOException e) {
+
+        } catch (Exception e) {
             logger.error(e.getMessage());
         }
     }
 
     private void serveStaticResource(HttpRequest request, HttpResponse response) throws IOException {
-        File file = ResourceResolver.getResource(request.getUrl());
+        File file = ResourceResolver.getResource(request.getPath());
         response.setBody(file);
         response.setContentType(MimeType.getMimeType(file.getName()));
         response.setContentLength(file.length());
@@ -49,7 +60,21 @@ public class RequestHandler implements Runnable {
     private boolean requestStaticResource(HttpRequest request) throws IOException {
         if(!"GET".equals(request.getMethod())) return false;
 
-        File file = ResourceResolver.getResource(request.getUrl());
+        File file = ResourceResolver.getResource(request.getPath());
         return file.exists() && file.isFile();
+    }
+
+    private void handleServlet(HttpRequest request, HttpResponse response) throws Exception {
+        try {
+            ServletInfo servletInfo = servletManager.getServlet(request.getPath(), request.getMethod());
+            String servletClassName = servletInfo.className();
+            Class<HttpServlet> servletClass = (Class<HttpServlet>) Class.forName(servletClassName);
+            Object servlet = servletClass.getDeclaredConstructor().newInstance();
+            Method serviceMethod = servletClass.getSuperclass().getDeclaredMethod("service", HttpRequest.class, HttpResponse.class);
+            serviceMethod.setAccessible(true);
+            serviceMethod.invoke(servlet, request, response);
+        } catch (ClassNotFoundException | NoSuchMethodException |InvocationTargetException | IllegalAccessException e) {
+            logger.error(e.getMessage());
+        }
     }
 }
