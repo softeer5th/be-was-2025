193dea506d5138f1f32391ee6cece951efbe72f0
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-09 22:53:51 +0900

    feat: 디스패처 서블릿 구현
    - 동적 리소스를 처리하는 서블릿
    - 동적 리소스 처리 관련 예외도 해당 서블릿에서 처리
    - 정적 리소스의 처리는 서블릿 매니저에게 책임 위임

--- src/main/java/servlet/ServletManager.java
+++ src/main/java/servlet/ServletManager.java
@@ -1,22 +1,31 @@
 package servlet;
 
+import controller.HomeController;
+import controller.SignUpController;
 import exception.FileNotSupportedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import webserver.httpserver.HttpRequest;
 import webserver.httpserver.HttpResponse;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServletManager {
     private static final String NOT_FOUND = "NOT_FOUND";
     private static final String FILE_NOT_SUPPORTED = "FILE_NOT_SUPPORTED";
     private static final String BAD_REQUEST = "BAD_REQUEST";
-    public static final String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+    private static final String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+    private static final String DISPATCHER = "dispatcher";
+    private static final String DEFAULT = "default";
+    private static final Logger log = LoggerFactory.getLogger(ServletManager.class);
     private final Map<String, Servlet> servlets = new HashMap<>();
-    public ServletManager() {
+    public ServletManager(List<Object> controllers) {
         servlets.put("default", new StaticResourceServlet());
+        servlets.put("dispatcher", new DispatcherServlet(controllers));
         servlets.put(FILE_NOT_SUPPORTED, new FileNotSupportedErrorPageServlet());
         servlets.put(NOT_FOUND, new FileNotFoundPageServlet());
         servlets.put(BAD_REQUEST, new BadRequestServlet());
@@ -28,13 +37,10 @@ public class ServletManager {
     }
 
     public void serve(HttpRequest request, HttpResponse response) throws IOException {
-        String uri = request.getUri();
         try {
-            if (!servlets.containsKey(uri)) {
-                servlets.get("default").handle(request, response);
-                return;
+            if (!servlets.get(DISPATCHER).handle(request, response)) {
+                servlets.get(DEFAULT).handle(request, response);
             }
-            servlets.get(uri).handle(request, response);
         } catch (FileNotSupportedException e) {
             servlets.get(FILE_NOT_SUPPORTED).handle(request, response);
         } catch (FileNotFoundException e){
@@ -42,6 +48,7 @@ public class ServletManager {
         } catch (IOException | IllegalArgumentException e) {
             servlets.get(BAD_REQUEST).handle(request, response);
         } catch (Exception e) {
+            log.error(e.getMessage(), e);
             servlets.get(INTERNAL_SERVER_ERROR).handle(request, response);
         }
     }
