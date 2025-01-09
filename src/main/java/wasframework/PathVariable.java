73313500abf1a5ee99cfae307ab9044f4ee5f3fb
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-09 18:16:43 +0900

    docs: @PathVariable의 사전조건 설정
    - 반드시 변수명과 value 가 같도록 사전조건 명세

--- src/main/java/wasframework/PathVariable.java
+++ src/main/java/wasframework/PathVariable.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 반드시 변수명과 value가 같아야 함
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface PathVariable {
