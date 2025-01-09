5d2c44d4b9ace0770e59ae32e7143c538912ed71
vvsos1 <vvsos10@naver.com>
2025-01-09 13:53:18 +0900

    fix. read request body based on Content-Length header

--- src/main/java/webserver/request/HttpRequest.java
+++ src/main/java/webserver/request/HttpRequest.java
@@ -3,12 +3,16 @@ package webserver.request;
 
 import webserver.enums.HttpMethod;
 import webserver.enums.HttpVersion;
+import webserver.exception.BadRequest;
 import webserver.exception.HttpVersionNotSupported;
+import webserver.exception.InternalServerError;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
+import static webserver.enums.HttpHeader.CONTENT_LENGTH;
 
 // HTTP 요청과 관련된 정보를 담는 객체
 public class HttpRequest {
@@ -48,8 +52,31 @@ public class HttpRequest {
         return headers;
     }
 
-    public String getBodyAsString() {
-        return body.lines().collect(Collectors.joining());
+    // request body를 읽어들여 문자열로 반환
+    public String readBodyAsString() {
+        String contentLengthString = headers.get(CONTENT_LENGTH.value);
+        if (contentLengthString == null)
+            throw new BadRequest(CONTENT_LENGTH.value + "헤더가 없습니다.");
+
+        try {
+            int contentLength = Integer.parseInt(contentLengthString);
+            char[] buffer = new char[contentLength];
+            int remainBytes = contentLength;
+            // // Contetnt-Length 크기만큼 읽어들임
+            while (remainBytes > 0) {
+                int readBytes = body.read(buffer, contentLength - remainBytes, remainBytes);
+                if (readBytes == -1) {
+                    throw new BadRequest(CONTENT_LENGTH.value + "값이 올바르지 않습니다.");
+                }
+                remainBytes -= readBytes;
+            }
+            return new String(buffer);
+        } catch (NumberFormatException e) {
+            throw new BadRequest(CONTENT_LENGTH.value + "값이 올바르지 않습니다.");
+        } catch (IOException e) {
+            throw new InternalServerError("요청 Body를 읽는 중 오류가 발생했습니다.");
+        }
+
     }
 
     public void validateSupportedHttpVersion(List<HttpVersion> supportedVersions) {
