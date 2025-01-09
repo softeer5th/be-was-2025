21206c2b58787d47912fd9822295fba5a5f9283a
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-09 17:43:04 +0900

    feat: ControllerMethod Record로 선언

--- src/main/java/wasframework/ControllerMethod.java
+++ src/main/java/wasframework/ControllerMethod.java
@@ -2,12 +2,4 @@ package wasframework;
 
 import java.lang.reflect.Method;
 
-public class ControllerMethod {
-    private final Object controller;
-    private final Method method;
-
-    public ControllerMethod(Object controller, Method method) {
-        this.controller = controller;
-        this.method = method;
-    }
-}
+public record ControllerMethod (Object controller, Method method) {}
