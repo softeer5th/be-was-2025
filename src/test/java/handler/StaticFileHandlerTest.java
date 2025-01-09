aa23baacdd088dcdf25d81c4cf7a0e21696bfa8d
zoouniak <jueun1025@naver.com>
2025-01-09 11:44:46 +0900

    test : 실제 파일을 읽어 byte로 변환하도록 수정

--- src/test/java/handler/StaticFileHandlerTest.java
+++ src/test/java/handler/StaticFileHandlerTest.java
@@ -8,14 +8,18 @@ import org.junit.jupiter.api.Test;
 import request.RequestInfo;
 import response.HttpResponse;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 class StaticFileHandlerTest {
     private final StaticFileHandler staticFileHandler = new StaticFileHandler();
 
     @Test
-    @DisplayName("서버가 제공하는 유효한 정적 파일이면 200 요청과 함께 body에 파일의 내용을 보낸다.")
-    void handle() {
+    @DisplayName("서버가 제공하는 유효한 정적 파일이면 200 상태코드와 함께 body에 파일의 내용을 전송한다..")
+    void handle() throws IOException {
         RequestInfo request = new RequestInfo(HttpMethod.GET, "/test.html");
-        byte[] expected = "<h1>test</h1>".getBytes();
+        byte[] expected = Files.readAllBytes(new File("src/test/resources/test.html").toPath());
 
         HttpResponse response = staticFileHandler.handle(request);
 
@@ -23,6 +27,18 @@ class StaticFileHandlerTest {
                 .isEqualTo(HttpStatus.OK);
         Assertions.assertThat(response.getBody())
                 .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("서버가 제공하지 않는 정적파일이면 404 상태코드 응답을 전송한다.")
+    void handle_invalidFileRequest() {
+        RequestInfo request = new RequestInfo(HttpMethod.GET, "/invalid.html");
+
+        HttpResponse response = staticFileHandler.handle(request);
+
+        Assertions.assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.NOT_FOUND);
+
     }
 
 }
