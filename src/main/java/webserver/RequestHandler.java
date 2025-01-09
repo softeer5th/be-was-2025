c5522cd312a1cec13d2ce44be92194118ca950f1
mikekks <thdalsrb79@naver.com>
2025-01-09 16:58:47 +0900

    [step3] chore: 서블릿 프론트 컨트롤러 패턴 적용함에 따라 수정

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,31 +2,24 @@ package webserver;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import enums.ContentType;
-import enums.HttpHeader;
-import enums.HttpMethod;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
-import enums.HttpStatus;
+import http.servlet.FrontControllerServlet;
 
 public class RequestHandler implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-	private static final String STATIC_FILES_PATH = "src/main/resources/static";
+	private final Socket connection;
 
-	private Socket connection;
-
-	public RequestHandler(Socket connectionSocket) {
+	public RequestHandler(final Socket connectionSocket) {
 		this.connection = connectionSocket;
 	}
 
@@ -41,39 +34,22 @@ public class RequestHandler implements Runnable {
 		}
 	}
 
-	private void handleRequest(InputStream in, OutputStream out) throws IOException {
-		DataOutputStream dos = new DataOutputStream(out);
+	private void handleRequest(final InputStream in, final OutputStream out) throws IOException {
 
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			DataOutputStream dos = new DataOutputStream(out)) {
 			HttpRequest request = new HttpRequest(reader);
+			HttpResponse response = new HttpResponse();
 
-			if (HttpMethod.GET.equals(request.getMethod())) {
-				serveStaticFile(request, dos);
-			} else {
-				// response.send404(dos);
-			}
-		} catch (IOException e) {
-			logger.error("Failed to parse the request: {}", e.getMessage());
-			// response.send404();
-		}
-	}
-
-	private void serveStaticFile(HttpRequest request, DataOutputStream dos) throws IOException {
-		HttpResponse response = new HttpResponse();
-
-		File file = new File(STATIC_FILES_PATH + request.getPath());
-		if (file.exists() && file.isFile()) {
-			byte[] body = Files.readAllBytes(file.toPath());
-			ContentType contentType = request.inferContentType();
-
-			response.setStatusCode(HttpStatus.OK);
-			response.setVersion(request.getVersion());
-			response.setHeader(HttpHeader.CONTENT_TYPE.getValue(), contentType.getMimeType());
-			response.setBody(body);
+			FrontControllerServlet frontControllerServlet = FrontControllerServlet.getInstance();
+			frontControllerServlet.service(request, response);
 
 			response.sendResponse(dos);
-		} else {
-			//response.send404();
+
+		} catch (IOException e) {
+			// TODO: 예외 처리 필요.
+
+			logger.error("Failed to parse the request: {}", e.getMessage());
 		}
 	}
 }
