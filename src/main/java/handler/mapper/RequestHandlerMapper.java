2f5b9fef1fbbc2e68d1e6a75b5ba8d1763ff0614
khw7385 <khw7385@naver.com>
2025-01-09 18:30:32 +0900

    refactor: 싱글톤 구현 부분 제거

--- src/main/java/handler/mapper/RequestHandlerMapper.java
+++ src/main/java/handler/mapper/RequestHandlerMapper.java
@@ -7,12 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RequestHandlerMapper {
-    private static final RequestHandlerMapper INSTANCE = new RequestHandlerMapper();
     private final Map<String, RequestHandler> requestHandlerMap = new HashMap<>();
 
-    public static RequestHandlerMapper getInstance(){
-        return INSTANCE;
-    }
     public RequestHandlerMapper(){
         requestHandlerMap.put("/create", new UserRequestHandler());
     }
