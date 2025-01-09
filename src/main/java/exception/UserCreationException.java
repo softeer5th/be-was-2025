85854665b8ba6212507fb7a234a812403ee4e2ca
codrin2 <hssang127@gmail.com>
2025-01-09 11:50:57 +0900

    feat(UserCreationException): 유저 생성 관련 예외 추가

---
+++ src/main/java/exception/UserCreationException.java
@@ -0,0 +1,17 @@
+package exception;
+
+public class UserCreationException extends RuntimeException {
+    private final ErrorCode errorCode;
+    public UserCreationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return "[%s] %s".formatted(errorCode.name(), super.getMessage());
+    }
+}
