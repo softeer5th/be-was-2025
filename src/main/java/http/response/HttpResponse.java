97c863e837d42ee195b0fab02fd67257da8e34b8
성필 <philip000412@gmail.com>
2025-01-09 18:34:10 +0900

    feat: HTTP Response를 반환하는 클래스 생성

---
+++ src/main/java/http/response/HttpResponse.java
@@ -0,0 +1,49 @@
+package http.response;
+
+import http.enums.HttpVersion;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import http.enums.HttpResponseStatus;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class HttpResponse {
+    private static final Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+    private final OutputStream out;
+    private DataOutputStream dos;
+
+    public HttpResponse(OutputStream out) {
+        this.out = out;
+        dos = new DataOutputStream(out);
+    }
+
+    public OutputStream getOutputStream() {
+        return out;
+    }
+
+    public void sendErrorResponse(HttpResponseStatus status) throws IOException {
+        String body = String.format("<h1>%s</h1>", status);
+        writeResponseHeader(status, ContentType.HTML.getMimeType(), body);
+        writeResponseBody(body);
+    }
+
+    public void sendSuccessResponse(HttpResponseStatus status, String type, String body) throws IOException {
+        writeResponseHeader(status, type, body);
+        writeResponseBody(body);
+    }
+
+    private void writeResponseHeader(HttpResponseStatus responseStatus, String contentType, String body) throws IOException {
+        logger.debug("Output: " + responseStatus + " " + contentType);
+        dos.writeBytes(HttpVersion.HTTP_1_1.getVersion() + " " + responseStatus.getStatusCode() + " " + responseStatus.getStatusMessage() + " \r\n");
+        dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + body.getBytes("UTF-8").length + "\r\n");
+        dos.writeBytes("\r\n");
+    }
+
+    private void writeResponseBody(String body) throws IOException {
+        dos.write(body.getBytes("UTF-8"));
+        dos.flush();
+    }
+}
