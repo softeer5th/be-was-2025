35bdf1b3ce82205d63c8a96544fe5536f1cf6be9
codrin2 <hssang127@gmail.com>
2025-01-09 17:10:25 +0900

    feat(JsonUtil): json 형식으로 변경해주는 util 구현

---
+++ src/main/java/util/JsonUtil.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.util.Map;
+
+public class JsonUtil {
+
+    public static String toJson(Map<String, String> data) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        int cnt = 0;
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            if (cnt > 0) sb.append(",");
+            sb.append("\"").append(escape(entry.getKey())).append("\":");
+            sb.append("\"").append(escape(entry.getValue())).append("\"");
+            cnt++;
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+    private static String escape(String str) {
+        if (str == null) return "";
+        return str
+                .replace("\\", "\\\\")
+                .replace("\"", "\\\"");
+    }
+}
