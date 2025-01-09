8d0a16cca365331f096b568b9bc5f71fa5c8122b
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-09 12:11:46 +0900

    feat: 회원가입 컨트롤러 구현
    - 회원 가입과 관련된 HTTP 응답 핸들러를 하나의 컨트롤러로 합침

---
+++ src/main/java/controller/SignUpController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.httpserver.HttpMethod;
+import webserver.httpserver.HttpRequest;
+import webserver.httpserver.HttpResponse;
+import webserver.httpserver.StatusCode;
+
+import java.io.File;
+import java.io.IOException;
+
+import static db.Database.addUser;
+import static utils.FileUtils.getFile;
+
+public class SignUpController {
+
+    private static final Logger log = LoggerFactory.getLogger(SignUpController.class);
+
+    @Mapping(path = "/registration", method = HttpMethod.GET)
+    public void registerPage(HttpRequest request, HttpResponse response) throws IOException {
+        response.setStatusCode(StatusCode.OK);
+        response.setHeader("Content-Type", "text/html");
+
+        File file = new File("src/main/resources/static/registration/index.html");
+        byte[] readFile = getFile(file);
+        response.setBody(readFile);
+    }
+
+    @Mapping(path = "/create", method = HttpMethod.GET)
+    public void createUser(HttpRequest request, HttpResponse response) throws IOException {
+        String userId = request.getParameter("userId");
+        String password = request.getParameter("password");
+        String name = request.getParameter("name");
+        String email = null;
+        User user = new User(userId, password, name, email);
+        addUser(user);
+
+        log.info("User created: " + user);
+        response.setStatusCode(StatusCode.SEE_OTHER);
+        response.setHeader("Location", "/success");
+    }
+
+    @Mapping(path = "/success", method = HttpMethod.GET)
+    public void signUpSuccess(HttpRequest request, HttpResponse response) throws IOException {
+        response.setStatusCode(StatusCode.OK);
+        response.setHeader("Content-Type", "text/html");
+
+        File file = new File("src/main/resources/static/registration/success.html");
+        byte[] readFile = getFile(file);
+        response.setBody(readFile);
+    }
+}
