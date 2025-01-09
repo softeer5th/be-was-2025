cf7acd90791566164f64ff0666a3862a3d1f1499
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-09 12:12:07 +0900

    feat: 홈 컨트롤러 구현
    - 홈 페이지와 관련된 핸들러 메소드를 하나의 컨트롤러로 구현

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import webserver.httpserver.HttpMethod;
+import webserver.httpserver.HttpRequest;
+import webserver.httpserver.HttpResponse;
+import webserver.httpserver.StatusCode;
+
+import java.io.File;
+import java.io.IOException;
+
+import static utils.FileUtils.getFile;
+
+public class HomeController {
+
+    @Mapping(path = "/", method = HttpMethod.GET)
+    public void handle(HttpRequest request, HttpResponse response) throws IOException {
+        response.setStatusCode(StatusCode.OK);
+        response.setHeader("Content-Type", "text/html");
+
+        File file = new File("src/main/resources/static/index.html");
+        byte[] readFile = getFile(file);
+        response.setBody(readFile);
+    }
+}
