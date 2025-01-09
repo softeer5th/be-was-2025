6410e105ab0270803f022d9831a37737756d5289
성필 <philip000412@gmail.com>
2025-01-09 18:33:08 +0900

    refactor: 각 요청별로 적절한 핸들러가 작업을 처리하도록 구조 수정

---
+++ src/main/java/http/handler/NotFoundHandler.java
@@ -0,0 +1,23 @@
+package http.handler;
+
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+import http.enums.HttpResponseStatus;
+import http.response.ContentType;
+
+import java.io.IOException;
+
+public class NotFoundHandler implements Handler {
+    private static NotFoundHandler instance = new NotFoundHandler();
+
+    private NotFoundHandler() {}
+
+    public static NotFoundHandler getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void handle(HttpRequest request, HttpResponse response) throws IOException {
+        response.sendErrorResponse(HttpResponseStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/http/handler/StaticResourceHandler.java
@@ -0,0 +1,47 @@
+package http.handler;
+
+import http.request.HttpRequest;
+import util.HttpRequestUtil;
+import http.response.HttpResponse;
+import http.request.TargetInfo;
+import http.response.ContentType;
+import util.FileUtil;
+import http.enums.HttpResponseStatus;
+
+import java.io.IOException;
+
+public class StaticResourceHandler implements Handler {
+    private static final String STATIC_RESOURCE_PATH = "./src/main/resources/static";
+    private static final String NOT_FOUND_MESSAGE = "<h1>404 Not Found</h1>";
+
+    private static StaticResourceHandler instance = new StaticResourceHandler();
+
+    private StaticResourceHandler() {}
+
+    public static StaticResourceHandler getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void handle(HttpRequest request, HttpResponse response) {
+        TargetInfo target = request.getTarget();
+        String path = STATIC_RESOURCE_PATH + target.getPath();
+        System.out.println(path);
+
+        path = HttpRequestUtil.buildPath(path);
+        String type = HttpRequestUtil.getType(path); // 파일 유형 별로 Content-Type 할당
+
+        String body; // 해당 파일의 경로를 byte로 전달
+        try {
+            byte[] file = FileUtil.fileToByteArray(path);
+            if (file != null) {
+                body = new String(file);
+                response.sendSuccessResponse(HttpResponseStatus.OK, type, body);
+            } else {
+                response.sendErrorResponse(HttpResponseStatus.NOT_FOUND);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
