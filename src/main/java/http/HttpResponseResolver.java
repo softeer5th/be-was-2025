5e68c21b7692e8c3c9a82330932d75bb553fd689
khw7385 <khw7385@naver.com>
2025-01-10 11:02:55 +0900

    feat(HttpResponseResolver): http 응답 전송 메소드 단일화  HttpResponse 클래스를 생성함으로써 더 이상 응답 종류에 따라 메소드를 구분하지 않고 하나의 메소드로 처리할 수 있게 됨

--- src/main/java/http/HttpResponseResolver.java
+++ src/main/java/http/HttpResponseResolver.java
@@ -6,6 +6,7 @@ import util.FileUtil;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.Map;
 
 public class HttpResponseResolver {
     private static final HttpResponseResolver INSTANCE = new HttpResponseResolver();
@@ -16,42 +17,25 @@ public class HttpResponseResolver {
     public HttpResponseResolver(){
     }
 
-    public void sendResponse(DataOutputStream dos, HttpStatus httpStatus, String path, byte[] data) throws IOException {
-        String mimeType = MimeType.TEXT_PLAIN.getMimeType();
-
-        if(httpStatus == HttpStatus.OK){
-            String extension = FileUtil.extractFileExtension(path);
-            mimeType = MimeType.getMimeType(extension);
-        }
-
-        writeGeneralHeader(dos, httpStatus, mimeType, data.length);
-        writeBody(dos, data);
-    }
-
-    public void sendRedirectResponse(DataOutputStream dos, HttpStatus status, String location) throws IOException {
-        writeRedirectHeader(dos, status, location);
-    }
-
-    private void writeGeneralHeader(DataOutputStream dos, HttpStatus httpStatus, String contentType, int contentLength) throws IOException {
-        writeStatusLine(dos, httpStatus);
-        dos.writeBytes(String.format("Content-Type: %s;charset=utf-8\r\n", contentType));
-        dos.writeBytes(String.format("Content-Length: %d\r\n", contentLength));
-        dos.writeBytes("\r\n");
-    }
-
-    private void writeRedirectHeader(DataOutputStream dos, HttpStatus status, String location) throws IOException{
-        writeStatusLine(dos, status);
-        dos.writeBytes(String.format("Location: %s\r\n", location));
-        dos.writeBytes("Content-Length: 0\r\n");
-        dos.writeBytes("\r\n");
-    }
-
-    private void writeBody(DataOutputStream dos, byte[] data) throws IOException {
-        dos.write(data, 0, data.length);
-        dos.flush();
+    public void sendResponse(DataOutputStream dos, HttpResponse httpResponse) throws IOException {
+        writeStatusLine(dos, httpResponse.getHttpStatus());
+        writeHeaders(dos, httpResponse.getHeaders());
+        writeBody(dos, httpResponse.getBody());
     }
 
     private void writeStatusLine(DataOutputStream dos, HttpStatus httpStatus) throws IOException {
         dos.writeBytes(String.format("HTTP/1.1 %d %s\r\n", httpStatus.getStatusCode(), httpStatus.getReasonPhrase()));
+    }
+
+    private void writeHeaders(DataOutputStream dos, Map<String, String> headers) throws IOException {
+        for(String headerKey: headers.keySet()){
+            dos.writeBytes(String.format("%s: %s\r\n", headerKey, headers.get(headerKey)));
+        }
+        dos.writeBytes("\r\n");
+    }
+
+    private void writeBody(DataOutputStream dos, byte[] body) throws IOException {
+        dos.write(body, 0, body.length);
+        dos.flush();
     }
 }
