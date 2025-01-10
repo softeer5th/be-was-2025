bda10d777366cc341151c836ee34f6a7f081bf87
scc9811 <wjsrudtjr98@naver.com>
2025-01-10 14:27:59 +0900

    test : 테스트코드 BDD 스타일으로 수정

--- src/test/java/db/DatabaseTest.java
+++ src/test/java/db/DatabaseTest.java
@@ -4,6 +4,8 @@ import model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -12,18 +14,30 @@ class DatabaseTest {
     @Test
     @DisplayName("ID로 사용자 조회")
     void findUserById() {
+        // given
         User user1 = new User("u1", "1234", "user1", "u1@gmail.com");
         Database.addUser(user1);
-        assertThat(user1).isEqualTo(Database.findUserById("u1"));
+
+        // when
+        User foundUser = Database.findUserById("u1");
+
+        // then
+        assertThat(user1).isEqualTo(foundUser);
     }
 
     @Test
     @DisplayName("현재 저장된 사용자 조회")
     void findAll() {
+        // given
         User user1 = new User("u1", "1234", "user1", "u1@gmail.com");
         User user2 = new User("u2", "4321", "user2", "u2@gmail.com");
         Database.addUser(user1);
         Database.addUser(user2);
-        assertThat(Database.findAll()).contains(user1, user2);
+
+        // when
+        Collection<User> users = Database.findAll();
+
+        // then
+        assertThat(users).contains(user1, user2);
     }
 }
