4ccfa3840e58c5e045e36530fe6107d36abf6fd8
scc9811 <wjsrudtjr98@naver.com>
2025-01-10 11:56:13 +0900

    refactor : Http 상태(코드) 추가

--- src/main/java/http/HttpStatus.java
+++ src/main/java/http/HttpStatus.java
@@ -3,7 +3,11 @@ package http;
 public enum HttpStatus {
     OK("200", "OK"),
     CREATED("201", "Created"),
-    NOT_FOUND("404", "Not Found");
+    NOT_FOUND("404", "Not Found"),
+    BAD_REQUEST("400", "Bad Request"),
+    UNAUTHORIZED("401", "Unauthorized"),
+    CONFLICT("409", "Conflict"),
+    INTERNAL_SERVER_ERROR("500", "Internal Server Error");
 
     private final String code;
     private final String status;
