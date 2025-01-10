abdd2e2b230cac024956759ed5cc5c20bed4397b
khw7385 <khw7385@naver.com>
2025-01-10 10:59:07 +0900

    feat(FileRequestHandler): FileRequestHandler 구현

---
+++ src/main/java/handler/FileRequestHandler.java
@@ -0,0 +1,30 @@
+package handler;
+
+import http.HttpRequest;
+import http.HttpResponse;
+import http.enums.HttpMethod;
+import http.enums.HttpStatus;
+import http.enums.MimeType;
+import util.FileUtil;
+
+import java.io.File;
+
+public class FileRequestHandler implements RequestHandler{
+    @Override
+    public boolean canHandle(HttpRequest httpRequest) {
+        if(httpRequest.getMethod() == HttpMethod.GET && FileUtil.isFileExist(httpRequest.getPath())){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public HttpResponse handle(HttpRequest httpRequest) {
+        File file = FileUtil.getFile(httpRequest.getPath());
+
+        String extension = FileUtil.extractFileExtension(file.getPath());
+        byte[] data = FileUtil.readFileToByteArray(file);
+
+        return new HttpResponse(HttpStatus.OK, MimeType.getMimeType(extension), data);
+    }
+}
