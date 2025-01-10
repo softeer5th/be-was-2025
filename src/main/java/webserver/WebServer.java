be476706dd79fb105b5a8f77db41ea1f1d7da05e
yulee <dbfl0461@gmail.com>
2025-01-10 10:49:24 +0900

    [Refact] port 정하는 코드 private 메소드로 분리

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -7,32 +7,43 @@ import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import router.RequestRouter;
+import router.Router;
 
 public class WebServer {
+
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
     private static final int THREAD_POOL_SIZE = Runtime.getRuntime().availableProcessors();
 
-    public static void main(String args[]) throws Exception {
-        int port = 0;
-        if (args == null || args.length == 0) {
-            port = DEFAULT_PORT;
-        } else {
-            port = Integer.parseInt(args[0]);
-        }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
+    public static void main(String args[]) throws Exception {
+        int port = determinePort(args);
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        Router router = new RequestRouter();
+
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
-
-            // concurrent 패키지로 스레드풀 생성
-            ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                executorService.submit(new RequestHandler(connection));
+                executorService.submit(new RequestHandler(connection, router));
             }
+            executorService.shutdown();
         }
     }
+
+    private static int determinePort(String[] args) {
+        if (args == null || args.length == 0) {
+            return DEFAULT_PORT;
+        }
+        try {
+            return Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            logger.warn("잘못된 port 입니다. 기본 포트를 사용합니다. {}", DEFAULT_PORT);
+            return DEFAULT_PORT;
+        }
+    }
+
 }
