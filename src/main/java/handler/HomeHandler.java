4059d0db302629f855f63a4731b0d8ad5824bcec
zoouniak <jueun1025@naver.com>
2025-01-10 12:23:51 +0900

    refactor : 홈 url 환경변수로 변경

--- src/main/java/handler/HomeHandler.java
+++ src/main/java/handler/HomeHandler.java
@@ -9,16 +9,15 @@ import request.HttpRequestInfo;
 import response.HttpResponse;
 
 public class HomeHandler implements Handler {
-    private static final String HOME_PATH = "http://localhost:8080/index.html";
+    private static final String HOME_URL = System.getenv("HOME_URL");
 
     @Override
     public HttpResponse handle(HttpRequestInfo request) {
         if (!request.getMethod().equals(HttpMethod.GET)) {
             throw new ClientErrorException(ErrorCode.INVALID_HTTP_METHOD);
         }
-
         HttpResponse response = new HttpResponse(HttpStatus.SEE_OTHER, FileContentType.HTML_UTF_8, "");
-        response.setHeaders("Location", HOME_PATH);
+        response.setHeaders("Location", HOME_URL);
         return response;
     }
 }
