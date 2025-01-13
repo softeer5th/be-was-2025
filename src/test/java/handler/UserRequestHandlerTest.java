8774b943b804fb9a2dfafa744a7a99d2ed487e2b
zoouniak <jueun1025@naver.com>
2025-01-13 18:17:41 +0900

    test : get 요청 시 실패 테스트

--- src/test/java/handler/UserRequestHandlerTest.java
+++ src/test/java/handler/UserRequestHandlerTest.java
@@ -3,6 +3,8 @@ package handler;
 import enums.HttpMethod;
 import enums.HttpStatus;
 import enums.HttpVersion;
+import exception.ClientErrorException;
+import exception.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import request.HttpRequestInfo;
@@ -11,6 +13,7 @@ import response.HttpResponse;
 import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserRequestHandlerTest {
     private final UserRequestHandler userRequestHandler = new UserRequestHandler();
@@ -20,11 +23,22 @@ class UserRequestHandlerTest {
     @Test
     @DisplayName("회원가입에 성공하면 201을 반환한다.")
     void handle_createUser() {
-        HttpRequestInfo httpRequestInfo = new HttpRequestInfo(HttpMethod.GET, VALID_REQUEST_PATH, HttpVersion.HTTP1_1, new HashMap<>(), null);
+        HttpRequestInfo httpRequestInfo = new HttpRequestInfo(HttpMethod.POST, "/user/create", HttpVersion.HTTP1_1, new HashMap<>(), "userId=jueun&nickname=jueun&password=jueun&email=jueun@naver.com");
         HttpResponse response = userRequestHandler.handle(httpRequestInfo);
 
         assertThat(response.getStatus())
-                .isEqualTo(HttpStatus.CREATED);
+                .isEqualTo(HttpStatus.FOUND);
+        assertThat(response.getHeaderValue("Location"))
+                .isEqualTo("http://localhost:8080/index.html");
     }
 
+    @Test
+    @DisplayName("GET으로 회원가입 요청 시 에러가 발생한다.")
+    void handle_createUser_GET(){
+        HttpRequestInfo httpRequestInfo = new HttpRequestInfo(HttpMethod.GET, VALID_REQUEST_PATH, HttpVersion.HTTP1_1, new HashMap<>(), null);
+        assertThatThrownBy(()-> userRequestHandler.handle(httpRequestInfo))
+                .isInstanceOf(ClientErrorException.class)
+                .hasMessage(ErrorCode.METHOD_NOT_ALLOWED.getMessage());
+
+    }
 }
