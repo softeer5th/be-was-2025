3dbe96b9b104efc22e1abe001d772cbc4cbf0a59
zoouniak <jueun1025@naver.com>
2025-01-13 17:35:21 +0900

    feat : post로 회원가입 및 중복 아이디 체크

--- src/main/java/handler/UserRequestHandler.java
+++ src/main/java/handler/UserRequestHandler.java
@@ -2,7 +2,9 @@ package handler;
 
 import db.Database;
 import enums.FileContentType;
+import enums.HttpHeader;
 import enums.HttpStatus;
+import exception.ClientErrorException;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +12,13 @@ import request.HttpRequestInfo;
 import request.UserCreateRequest;
 import response.HttpResponse;
 
-import static enums.HttpMethod.GET;
+import static enums.HttpMethod.POST;
+import static exception.ErrorCode.ALREAD_EXIST_USERID;
 
 public class UserRequestHandler implements Handler {
     private static final Logger logger = LoggerFactory.getLogger(UserRequestHandler.class);
     private static final String USER_REQUEST_PREFIX = "/user/";
+    private static final String REDIRECT_URL = "http://localhost:8080/index.html";
 
     @Override
     public HttpResponse handle(final HttpRequestInfo request) {
@@ -24,17 +28,22 @@ public class UserRequestHandler implements Handler {
         String path = request.getPath().substring(USER_REQUEST_PREFIX.length());
         HttpResponse response = new HttpResponse();
 
-        if (request.getMethod().equals(GET) && path.startsWith("create?")) {
-            UserCreateRequest userCreateRequest = UserCreateRequest.of(path.substring("create?".length()));
+        if (request.getMethod().equals(POST) && path.startsWith("create")) {
+            UserCreateRequest userCreateRequest = UserCreateRequest.of((String) request.getBody());
+
             createUser(userCreateRequest);
 
-            response.setResponse(HttpStatus.CREATED, FileContentType.HTML_UTF_8, "successfully created.!");
+            response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, "successssssss");
+            response.setHeaders(HttpHeader.LOCATION.getName(), REDIRECT_URL);
         }
 
         return response;
     }
 
     private void createUser(final UserCreateRequest request) {
+        if (Database.findUserById(request.userId()) == null)
+            throw new ClientErrorException(ALREAD_EXIST_USERID);
+
         User user = new User(request.userId(),
                 request.password(),
                 request.nickname(),
