f7a61dd5ca3b0c9ad8694e5740ee188c76e786ad
kwon204 <dafeg3@naver.com>
2025-01-14 11:14:09 +0900

    feat: 회원 가입 시 회원 정보 저장 후 리다이렉션 기능 추가
    body의 내용을 파싱하는 기능 추가

--- src/main/java/http/UserHandler.java
+++ src/main/java/http/UserHandler.java
@@ -5,6 +5,7 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class UserHandler {
@@ -15,21 +16,32 @@ public class UserHandler {
 
     public void createUser(HttpRequest request, HttpResponse response) {
         String redirectPath = "/registration";
-        Map<String, String> queries = request.getQueries();
-        String userId = queries.get("userId");
-        String username = queries.get("username");
-        String password = queries.get("password");
+        String body = request.getBody();
+        Map<String, String> data = parseBody(body);
+        String userId = data.get("userId");
+        String username = data.get("username");
+        String password = data.get("password");
         if (userId == null || username == null || password == null || Database.findUserById(userId) != null) {
             response.writeStatusLine(HttpStatus.SEE_OTHER);
             response.writeHeader("Location", redirectPath);
             response.send();
             return;
         }
-        User user = new User(queries.get("userId"), queries.get("username"), queries.get("password"), null);
-        redirectPath = "/login";
+        User user = new User(userId, username, password, null);
+        redirectPath = "/";
         Database.addUser(user);
         response.writeStatusLine(HttpStatus.SEE_OTHER);
         response.writeHeader("Location", redirectPath);
         response.send();
+    }
+
+    private Map<String, String> parseBody(String body) {
+        Map<String, String> map = new HashMap<>();
+        String[] tokens = body.split("&");
+        for(String token: tokens) {
+            String[] values = token.split("=");
+            map.putIfAbsent(values[0].trim(), values[1].trim());
+        }
+        return map;
     }
 }
