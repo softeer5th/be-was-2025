65481aa472a4f0287800cb1329c4993ea9893441
codrin2 <hssang127@gmail.com>
2025-01-14 15:46:14 +0900

    feat(UserCreationHandler): 요구사항 변경에 따른 회원가입 API HTTP Method POST로 적용

--- src/main/java/api/user/UserCreationHandler.java
+++ src/main/java/api/user/UserCreationHandler.java
@@ -5,75 +5,54 @@ import global.exception.ErrorCode;
 import global.exception.UserCreationException;
 import db.Database;
 import global.model.CommonResponse;
+import global.model.HttpRequest;
+import global.util.JsonUtil;
 import model.User;
-import global.model.RequestData;
 import global.model.LoadResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
 
 import static global.util.JsonUtil.toJson;
 
 public class UserCreationHandler implements ApiHandler {
+    private static final Logger logger = LoggerFactory.getLogger(UserCreationHandler.class);
 
     @Override
-    public boolean canHandle(RequestData requestData) {
-        return "GET".equalsIgnoreCase(requestData.method())
-                && requestData.path().startsWith("/api/create");
+    public boolean canHandle(HttpRequest httpRequest) {
+        return "POST".equalsIgnoreCase(httpRequest.method())
+                && httpRequest.path().startsWith("/api/create");
     }
 
     @Override
-    public LoadResult handle(RequestData requestData) {
+    public LoadResult handle(HttpRequest httpRequest) {
         User user;
         try {
-            String queryString = extractQueryString(requestData.path());
-            user = createUserFromQuery(queryString);
+            user = createUserFromBody(httpRequest.body());
             validateUser(user);
         } catch (UserCreationException e) {
             return handleSignupFailure(e);
         }
         Database.addUser(user);
-        return createSuccessResponse();
+        logger.debug("유저가 DB에 추가되었습니다.");
+        return createRedirectResponse();
     }
 
-    private String extractQueryString(String path) {
-        String[] splitQuestion = path.split("\\?", 2);
-        if (splitQuestion.length < 2) {
-            throw new UserCreationException(ErrorCode.INVALID_USER_INPUT);
-        }
-        return splitQuestion[1];
-    }
+    private User createUserFromBody(String body) {
+        Map<String, String> requestData = JsonUtil.fromJson(body);
 
-    private User createUserFromQuery(String queryString) {
-        Map<String, String> parameters = parseQueryString(queryString);
-
-        String userId = parameters.get("userId");
-        String password = parameters.get("password");
-        String name = parameters.get("name");
+        String userId = requestData.get("userId");
+        String password = requestData.get("password");
+        String name = requestData.get("name");
 
         if (userId == null || password == null || name == null) {
+            logger.error("회원가입에 필요한 모든 값이 입력되지 않았습니다.");
             throw new UserCreationException(ErrorCode.INVALID_USER_INPUT);
         }
 
-        return User.of(userId, password, name);
-    }
-
-    private Map<String, String> parseQueryString(String queryString) {
-        Map<String, String> parameters = new HashMap<>();
-        String[] params = queryString.split("&");
-
-        for (String param : params) {
-            String[] kv = param.split("=", 2);
-            if (kv.length < 2) continue;
-
-            String key = kv[0];
-            String value = URLDecoder.decode(kv[1], StandardCharsets.UTF_8);
-            parameters.put(key, value);
-        }
-
-        return parameters;
+        return new User(userId, password, name,null);
     }
 
     private void validateUser(User user) {
@@ -86,6 +65,7 @@ public class UserCreationHandler implements ApiHandler {
         if (nameExists) {
             throw new UserCreationException(ErrorCode.DUPLICATED_NAME);
         }
+        logger.debug("유저 생성 검증에 성공하였습니다.");
     }
 
     private LoadResult handleSignupFailure(UserCreationException e) {
@@ -100,9 +80,7 @@ public class UserCreationHandler implements ApiHandler {
         return new LoadResult(json.getBytes(StandardCharsets.UTF_8), "/api/create", "application/json");
     }
 
-    private LoadResult createSuccessResponse() {
-        CommonResponse commonResponse = new CommonResponse(true, null, null, null);
-        String json = toJson(commonResponse);
-        return new LoadResult(json.getBytes(StandardCharsets.UTF_8), "/api/create", "application/json");
+    private LoadResult createRedirectResponse() {
+        return new LoadResult(null, "/index.html", "redirect");
     }
 }
