2a8991287e0c055b793421fab7aed78ead7de7ac
efdao <efdao@naver.com>
2025-01-14 17:33:20 +0900

    Refactor: ParameterParser -> Parameter로 이름 변경

--- src/main/java/util/UserManager.java
+++ src/main/java/util/UserManager.java
@@ -4,12 +4,12 @@ import db.Database;
 import model.User;
 
 public class UserManager {
-    public void addUser(String parameter) throws IllegalArgumentException {
-        ParameterParser parameterParser = new ParameterParser(parameter);
-        String userId = parameterParser.getId();
-        String userName = parameterParser.getName();
-        String password = parameterParser.getPassword();
-        String email = parameterParser.getEmail();
+    public void addUser(String parameterString) throws IllegalArgumentException {
+        Parameter parameter = new Parameter(parameterString);
+        String userId = parameter.getId();
+        String userName = parameter.getName();
+        String password = parameter.getPassword();
+        String email = parameter.getEmail();
 
         if (Database.findUserById(userId) == null) {
             User user = new User(userId, userName, password, email);
