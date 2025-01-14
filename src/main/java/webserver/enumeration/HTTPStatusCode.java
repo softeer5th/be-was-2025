a1e636ea6a567d038bcaea1be704c47a110cf3a7
ji-woong-song <thdwldnd7@naver.com>
2025-01-14 14:37:17 +0900

    feat: HTTP Status code를 담은 exception 클래스 추가

--- src/main/java/webserver/enumeration/HTTPStatusCode.java
+++ src/main/java/webserver/enumeration/HTTPStatusCode.java
@@ -3,6 +3,7 @@ package webserver.enumeration;
 public enum HTTPStatusCode {
     OK(200, "OK"),
     CREATED(201, "Created"),
+    BAD_REQUEST(400, "Bad Request"),
     NOT_FOUND(404, "Not Found"),
     SERVER_ERROR(500, "Internal Server Error");
 
