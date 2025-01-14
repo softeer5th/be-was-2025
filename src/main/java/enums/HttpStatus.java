aa800b06ae8ae6477cd2aa1fc33e6f5d5cd70dac
zoouniak <jueun1025@naver.com>
2025-01-14 11:57:52 +0900

    feat : 500 에러 추가

--- src/main/java/enums/HttpStatus.java
+++ src/main/java/enums/HttpStatus.java
@@ -3,13 +3,13 @@ package enums;
 public enum HttpStatus {
     OK(200, "Ok"),
     CREATED(201, "Created"),
+    FOUND(302, "Found"),
+    SEE_OTHER(303, "See Other"),
     BAD_REQUEST(400, "Bad Request"),
     NOT_FOUND(404, "Not Found"),
     METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
     UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
-    FOUND(302, "Found"),
-    SEE_OTHER(303, "See Other");
-
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private final int code;
     private final String message;
