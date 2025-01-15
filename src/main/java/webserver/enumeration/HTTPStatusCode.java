695bbd6e9b348fca99017cdb983ec916e8282585
ji-woong-song <thdwldnd7@naver.com>
2025-01-15 14:59:16 +0900

    feat: 몇가지 400번대 상태코드 추가

--- src/main/java/webserver/enumeration/HTTPStatusCode.java
+++ src/main/java/webserver/enumeration/HTTPStatusCode.java
@@ -5,6 +5,12 @@ public enum HTTPStatusCode {
     CREATED(201, "Created"),
     BAD_REQUEST(400, "Bad Request"),
     NOT_FOUND(404, "Not Found"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    NOT_ACCEPTABLE(406, "Not Acceptable"),
+    LENGTH_REQUIRED(411, "Length Required"),
+    UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
     SERVER_ERROR(500, "Internal Server Error");
 
     final int code;
