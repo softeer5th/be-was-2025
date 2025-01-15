04e8d9b1babda654b6484392b128bd8b94e7cb5e
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-15 11:50:00 +0900

    feat: 세션 구현에 ConcurrentHashMap 사용
    - 멀티 스레드 상황에서 세션에 대한 스레드 안정성을 보장하기 위해 변경

--- src/main/java/wasframework/HttpSession.java
+++ src/main/java/wasframework/HttpSession.java
@@ -2,9 +2,10 @@ package wasframework;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HttpSession {
-    private static Map<String, String> session = new HashMap<>();
+    private static Map<String, String> session = new ConcurrentHashMap<>();
 
     public static void put(String key, String value) {
         session.put(key, value);
