3821ee23877d7431ca0ba3777d3df0429f693f51
zoouniak <jueun1025@naver.com>
2025-01-16 17:36:12 +0900

    refactor : swith expression으로 변경

--- src/main/java/handler/UserRequestHandler.java
+++ src/main/java/handler/UserRequestHandler.java
@@ -22,6 +22,8 @@ public class UserRequestHandler implements Handler {
     private static final String USER_REQUEST_PREFIX = "/user/";
     private static final String REDIRECT_URL = "http://localhost:8080/index.html";
     private static final String LOGIN_FAIL_URL = "http://localhost:8080/login/fail.html";
+
+
     private static final String SID = "SID";
 
     private final UserManager userManager;
@@ -34,43 +36,46 @@ public class UserRequestHandler implements Handler {
     public HttpResponse handle(final HttpRequestInfo request) {
         logger.debug("request : {} ", request);
 
-
         String path = request.getPath().substring(USER_REQUEST_PREFIX.length());
         HttpResponse response = new HttpResponse();
 
-        if (path.startsWith("create")) {
-            checkPostMethod(request.getMethod());
+        switch (path) {
+            case "create" -> {
+                checkPostMethod(request.getMethod());
 
-            UserCreateRequest userCreateRequest = UserCreateRequest.of((String) request.getBody());
+                UserCreateRequest userCreateRequest = UserCreateRequest.of((String) request.getBody());
 
-            userManager.createUser(userCreateRequest);
+                userManager.createUser(userCreateRequest);
 
-            response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, "successssssss");
-            response.setHeaders(HttpHeader.LOCATION.getName(), REDIRECT_URL);
-        } else if (path.startsWith("login")) {
-            checkPostMethod(request.getMethod());
-            UserLoginRequest userLoginRequest = UserLoginRequest.of((String) request.getBody());
+                response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, "registration success");
+                response.setHeaders(HttpHeader.LOCATION.getName(), REDIRECT_URL);
+            }
+            case "login" -> {
+                checkPostMethod(request.getMethod());
+                UserLoginRequest userLoginRequest = UserLoginRequest.of((String) request.getBody());
 
-            try {
-                final String sessionId = userManager.loginUser(userLoginRequest);
+                try {
+                    final String sessionId = userManager.loginUser(userLoginRequest);
 
-                response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, "login success");
+                    response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, "login success");
+                    response.setHeaders(
+                            HttpHeader.LOCATION.getName(), REDIRECT_URL,
+                            HttpHeader.SET_COOKIE.getName(), String.format("%s=%s; Path=/", SID, sessionId)
+                    );
+                } catch (LoginException e) {
+                    response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, e.getMessage());
+                    response.setHeaders(HttpHeader.LOCATION.getName(), LOGIN_FAIL_URL);
+                }
+            }
+            case "logout" -> {
+                userManager.logoutUser(request.getHeaderValue(HttpHeader.SET_COOKIE.getName()));
+
+                response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, "logout success");
                 response.setHeaders(
                         HttpHeader.LOCATION.getName(), REDIRECT_URL,
-                        HttpHeader.SET_COOKIE.getName(), String.format("%s=%s; Path=/", SID, sessionId)
+                        HttpHeader.SET_COOKIE.getName(), String.format("%s=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/", SID)
                 );
-            } catch (LoginException e) {
-                response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, e.getMessage());
-                response.setHeaders(HttpHeader.LOCATION.getName(), LOGIN_FAIL_URL);
             }
-        } else if (path.equals("logout")) {
-            userManager.logoutUser(request.getHeaderValue(HttpHeader.SET_COOKIE.getName()));
-
-            response.setResponse(HttpStatus.FOUND, FileContentType.HTML_UTF_8, "logoutSuccess");
-            response.setHeaders(
-                    HttpHeader.LOCATION.getName(), REDIRECT_URL,
-                    HttpHeader.SET_COOKIE.getName(), String.format("%s=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/", SID)
-            );
         }
 
         return response;
@@ -80,6 +85,4 @@ public class UserRequestHandler implements Handler {
         if (method != POST)
             throw new ClientErrorException(REQUEST_NOT_ALLOWED);
     }
-
-
 }
