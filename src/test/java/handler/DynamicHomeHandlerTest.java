6bdf0226c0dc1b9d5eea4c6f10e3bbbf8dad0745
zoouniak <jueun1025@naver.com>
2025-01-16 10:51:06 +0900

    test : 동적 홈화면 테스트

---
+++ src/test/java/handler/DynamicHomeHandlerTest.java
@@ -0,0 +1,54 @@
+package handler;
+
+import db.Database;
+import enums.HttpHeader;
+import enums.HttpMethod;
+import enums.HttpVersion;
+import model.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import request.HttpRequestInfo;
+import response.HttpResponse;
+import util.SessionManager;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DynamicHomeHandlerTest {
+    DynamicHomeHandler dynamicHomeHandler = new DynamicHomeHandler();
+
+    @Test
+    @DisplayName("로그인한 사용자가 홈화면을 접속할 경우 사용자의 이름이 나타난다.")
+    void handle_loginUser() {
+        SessionManager sessionManager = SessionManager.getInstance();
+        Database.addUser(new User("test", "test", "test", "test"));
+        final String sessionId = sessionManager.makeAndSaveSessionId("test");
+        final HttpRequestInfo request = new HttpRequestInfo(HttpMethod.GET, "/index.html", HttpVersion.HTTP1_1, Map.of(HttpHeader.COOKIE.getName(), String.format("SID=%s; Path=/", sessionId)), "");
+
+        final HttpResponse response = dynamicHomeHandler.handle(request);
+
+        final String body = new String(response.getBody(), StandardCharsets.UTF_8);
+        assertThat(body.contains("test님"))
+                .isTrue();
+    }
+
+    @Test
+    @DisplayName("로그인하지 않은 사용자가 홈화면을 접속할 경우 로그인, 회원가입 버튼이 나타난다.")
+    void handle_guest() {
+        final HttpRequestInfo request = new HttpRequestInfo(HttpMethod.GET, "/index.html", HttpVersion.HTTP1_1, new HashMap<>(), "");
+
+        final HttpResponse response = dynamicHomeHandler.handle(request);
+
+        final String body = new String(response.getBody(), StandardCharsets.UTF_8);
+        assertThat(body.contains("test님"))
+                .isFalse();
+        assertThat(body.contains("로그인"))
+                .isTrue();
+        assertThat(body.contains("회원 가입"))
+                .isTrue();
+    }
+
+}
