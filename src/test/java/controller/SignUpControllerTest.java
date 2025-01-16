94de63121b2501a5e753a33e9c9dd768bd42f69e
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-16 18:20:33 +0900

    refactor: 회원가입 테스트 코드 이동

---
+++ src/test/java/controller/SignUpControllerTest.java
@@ -0,0 +1,38 @@
+package controller;
+
+import model.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.httpserver.HttpRequest;
+import webserver.httpserver.HttpResponse;
+
+import java.io.IOException;
+
+import static db.Database.findUserById;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class SignUpControllerTest {
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void createUserTest() throws IOException {
+        HttpRequest request = mock(HttpRequest.class);
+        HttpResponse response = mock(HttpResponse.class);
+
+        when(request.getParameter("userId")).thenReturn("testUser");
+        when(request.getParameter("password")).thenReturn("testPassword");
+        when(request.getParameter("name")).thenReturn("testName");
+
+        SignUpController signUpController = new SignUpController();
+        signUpController.createUser(request, response);
+
+        User testUser = findUserById("testUser");
+        Assertions.assertThat(testUser.getPassword()).isEqualTo("testPassword");
+        Assertions.assertThat(testUser.getName()).isEqualTo("testName");
+
+        verify(response).setLocation("/");
+    }
+
+}
