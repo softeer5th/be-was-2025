634fec0962b2964d90b72de0d786f7a1f578d8df
efdao <efdao@naver.com>
2025-01-16 17:34:21 +0900

    Feat: 빌더 패턴의 SetCookie 클래스를 포함하는 CookieManager 추가

---
+++ src/main/java/webserver/cookie/CookieManager.java
@@ -0,0 +1,41 @@
+package webserver.cookie;
+
+public class CookieManager {
+
+    public static class SetCookie {
+        private final StringBuilder cookieBuilder;
+
+        public SetCookie(String name, String value) {
+            this.cookieBuilder = new StringBuilder();
+            this.cookieBuilder.append(name).append("=").append(value);
+        }
+
+        public SetCookie path(String path) {
+            if (path != null && !path.isBlank()) {
+                cookieBuilder.append("; Path=").append(path);
+            }
+            return this;
+        }
+
+        public SetCookie httpOnly() {
+            cookieBuilder.append("; HttpOnly");
+            return this;
+        }
+
+        public SetCookie secure() {
+            cookieBuilder.append("; Secure");
+            return this;
+        }
+
+        public SetCookie maxAge(int seconds) {
+            if (seconds > 0) {
+                cookieBuilder.append("; Max-Age=").append(seconds);
+            }
+            return this;
+        }
+
+        public String build() {
+            return cookieBuilder.toString();
+        }
+    }
+}
