9354443d6ca1715f134fbdabca39387900030a68
khw7385 <khw7385@naver.com>
2025-01-16 16:42:32 +0900

    feat: 로그아웃 구현

---
+++ src/main/java/handler/UserLogoutRequestHandler.java
@@ -0,0 +1,32 @@
+package handler;
+
+import http.cookie.Cookie;
+import http.enums.HttpStatus;
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+import http.session.Session;
+import http.session.SessionManager;
+
+public class UserLogoutRequestHandler implements RequestHandler{
+    private final SessionManager sessionManager = SessionManager.getInstance();
+
+    @Override
+    public boolean canHandle(HttpRequest httpRequest) {
+        return true;
+    }
+
+    @Override
+    public HttpResponse handle(HttpRequest httpRequest) {
+        Cookie cookie = httpRequest.getCookie("sessionId");
+
+        SessionManager.removeSession(cookie.getValue());
+
+        cookie.setMaxAge(0);
+
+        return new HttpResponse.Builder()
+                .httpStatus(HttpStatus.SEE_OTHER)
+                .location("http://localhost:8080/")
+                .setCookie(cookie)
+                .build();
+    }
+}
