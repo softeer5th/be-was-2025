2ac13f41e788bcd3b359911c372d1a050aba10a1
khw7385 <khw7385@naver.com>
2025-01-16 17:02:15 +0900

    feat: logout 에 대한 매핑 추가

--- src/main/java/handler/mapping/RequestHandlerMapping.java
+++ src/main/java/handler/mapping/RequestHandlerMapping.java
@@ -2,6 +2,7 @@ package handler.mapping;
 
 import handler.RequestHandler;
 import handler.UserLoginRequestHandler;
+import handler.UserLogoutRequestHandler;
 import handler.UserSignUpRequestHandler;
 import http.request.HttpRequest;
 import http.enums.HttpMethod;
@@ -16,6 +17,7 @@ public class RequestHandlerMapping {
     public void init(){
         handlerMap.put(new HandlerKey("/user/create", HttpMethod.POST), new UserSignUpRequestHandler());
         handlerMap.put(new HandlerKey("/login", HttpMethod.POST), new UserLoginRequestHandler());
+        handlerMap.put(new HandlerKey("/logout", HttpMethod.GET), new UserLogoutRequestHandler());
     }
 
     public Optional<RequestHandler> getHandler(HttpRequest httpRequest){
