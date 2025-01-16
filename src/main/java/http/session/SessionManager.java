25a5f95c8d7924a1eca5e4eb2b6733267821a556
khw7385 <khw7385@naver.com>
2025-01-16 16:28:06 +0900

    feat: SessionManager 생성

---
+++ src/main/java/http/session/SessionManager.java
@@ -0,0 +1,31 @@
+package http.session;
+
+import model.User;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SessionManager {
+    private static final SessionManager INSTANCE = new SessionManager();
+    private static final Map<String, Session> sessionStore = new ConcurrentHashMap<>();
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(10);
+
+    public static SessionManager getInstance(){
+        return INSTANCE;
+    }
+
+    private SessionManager(){}
+
+    public Session createSession(){
+        Session session = new Session();
+        sessionStore.put(session.getSessionId(), session);
+        scheduler.schedule(() -> {
+            sessionStore.remove(session.getSessionId());
+            }, Session.MAX_INACTIVE_INTERVAL, TimeUnit.MICROSECONDS);
+
+        return session;
+    }
+}
