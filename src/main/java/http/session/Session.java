dcfe3fbe2a397578feb5aae57c9601b520b98eca
khw7385 <khw7385@naver.com>
2025-01-16 13:38:11 +0900

    feat: Session 클래스 생성

--- src/main/java/http/session/Session.java
+++ src/main/java/http/session/Session.java
@@ -2,9 +2,20 @@ package http.session;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class Session {
+    private final String sessionId;
+    private final long createdTime;
+    private long lastAccessedTime;
+    private static final long MAX_INACTIVE_INTERVAL = 1000 * 60 * 30;
+
     private Map<String, Object> attributes = new HashMap<>();
+
+    public Session() {
+        sessionId = UUID.randomUUID().toString();
+        createdTime = System.currentTimeMillis();
+    }
 
     public boolean hasAttribute(String key){
         return attributes.containsKey(key);
@@ -17,4 +28,13 @@ public class Session {
     public void saveAttribute(String key, Object value){
         attributes.put(key, value);
     }
+
+    public void updateLastUpdatedTime(){
+        lastAccessedTime = System.currentTimeMillis();
+    }
+
+    public boolean isExpired(){
+        return (lastAccessedTime - System.currentTimeMillis()) >= MAX_INACTIVE_INTERVAL;
+    }
+
 }
