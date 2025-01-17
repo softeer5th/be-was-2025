aff6c012ba7759cccde3f73ffb91b7ac69eac007
popopy0412 <philip000412@gmail.com>
2025-01-17 12:16:53 +0900

    fix: 테스트 유저 정보 삭제

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -30,7 +30,6 @@ public class RequestHandler implements Runnable {
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-        Database.addUser(new User("asdf", "asdf", "asdfadfsadf", "asdf@asdf"));
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
