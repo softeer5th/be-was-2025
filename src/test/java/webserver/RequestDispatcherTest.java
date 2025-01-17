2c2fc096aa7c5d31b8539ee3091887b7c34dae47
zoouniak <jueun1025@naver.com>
2025-01-17 12:17:56 +0900

    test : server단 에러 catch 테스트

--- src/test/java/webserver/RequestDispatcherTest.java
+++ src/test/java/webserver/RequestDispatcherTest.java
@@ -1,6 +1,7 @@
 package webserver;
 
-import fixtureUtil.ExceptionRouter;
+import fixtureUtil.ClientExceptionRouter;
+import fixtureUtil.ServerExceptionRouter;
 import fixtureUtil.TestRouter;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -65,7 +66,7 @@ class RequestDispatcherTest {
     void run_clientException() throws IOException {
         // given
         Socket connection = Mockito.mock(Socket.class);
-        RequestDispatcher requestDispatcher = new RequestDispatcher(connection, new ExceptionRouter());
+        RequestDispatcher requestDispatcher = new RequestDispatcher(connection, new ClientExceptionRouter());
 
 
         String httpRequest = """
@@ -97,6 +98,53 @@ class RequestDispatcherTest {
                 Content-Type: text/html; charset=utf-8\r
                 \r
                 잘못된 HTTP 요청입니다.\r
+                """;
+
+        // when
+        requestDispatcher.run();
+
+        // then
+        Assertions.assertThat(mockOutputStream.toString())
+                .isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("핸들러에서 server exception을 던지면 4xx 응답을 클라이언트에 전송한다.")
+    void run_serverException() throws IOException {
+        // given
+        Socket connection = Mockito.mock(Socket.class);
+        RequestDispatcher requestDispatcher = new RequestDispatcher(connection, new ServerExceptionRouter());
+
+
+        String httpRequest = """
+                POST /test HTTP/1.1\r
+                Accept: application/json\r
+                Accept-Encoding: gzip, deflate\r
+                Connection: keep-alive\r
+                Content-Length: 4\r
+                Content-Type: application/json\r
+                Host: google.com\r
+                User-Agent: HTTPie/0.9.3\r
+                \r
+                gigi\r
+                """;
+
+        byte[] httpRequestBytes = httpRequest.getBytes();
+
+        InputStream mockInputStream = new ByteArrayInputStream(httpRequestBytes);
+        OutputStream mockOutputStream = new ByteArrayOutputStream();
+
+        when(connection.getInputStream())
+                .thenReturn(mockInputStream);
+        when(connection.getOutputStream())
+                .thenReturn(mockOutputStream);
+
+        final String expected = """
+                HTTP/1.1 500 Internal Server Error \r
+                Content-Length: 35\r
+                Content-Type: text/html; charset=utf-8\r
+                \r
+                인코딩에 실패하였습니다.\r
                 """;
 
         // when
