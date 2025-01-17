a4bb5676730c300e9868583ba934dc8d86ea2406
zoouniak <jueun1025@naver.com>
2025-01-17 12:29:46 +0900

    refactor: uuid로 변경

--- src/main/java/util/SessionManager.java
+++ src/main/java/util/SessionManager.java
@@ -2,13 +2,10 @@ package util;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
+import java.util.UUID;
 
 public class SessionManager {
     private final Map<String, String> sessionMap;
-
-    private static final String CHAR_POOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    private static final int STRING_LENGTH = 5;
     private static SessionManager instance;
 
     private SessionManager() {
@@ -23,15 +20,7 @@ public class SessionManager {
     }
 
     public String makeAndSaveSessionId(final String userId) {
-        Random random = new Random();
-        StringBuilder sessionBuilder = new StringBuilder(STRING_LENGTH);
-
-        for (int i = 0; i < STRING_LENGTH; i++) {
-            int randomIndex = random.nextInt(CHAR_POOL.length());
-            sessionBuilder.append(CHAR_POOL.charAt(randomIndex));
-        }
-
-        final String sessionId = sessionBuilder.toString();
+        final String sessionId = UUID.randomUUID().toString();
 
         sessionMap.put(sessionId, userId);
         return sessionId;
