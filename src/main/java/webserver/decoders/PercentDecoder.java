8084390069229523f9ee50668d329cbbaaea1a4b
ji-woong-song <thdwldnd7@naver.com>
2025-01-17 12:21:02 +0900

    fix: PercentDecoder 버그 해결 - +와 space를 같이 넣는 버그 - %vz와 같은 입력에 대해서 입력을 무시하는 버그 해결

--- src/main/java/webserver/decoders/PercentDecoder.java
+++ src/main/java/webserver/decoders/PercentDecoder.java
@@ -14,15 +14,19 @@ public class PercentDecoder implements ByteDecoder {
             if (bytes[i] == PLUS) {
                 out.write(SP);
             }
-            if (bytes[i] == PERCENT && i + 2 < bytes.length) {
+            else if (bytes[i] == PERCENT && i + 2 < bytes.length) {
                 byte hex1 = bytes[i + 1];
                 byte hex2 = bytes[i + 2];
                 if (isHexNumber(hex1) && isHexNumber(hex2)) {
                     String hexString = new String(new byte[]{hex1, hex2}, StandardCharsets.US_ASCII);
                     byte hexValue = Byte.parseByte(hexString, 16);
                     out.write(hexValue);
-                    i = i + 2;
+                } else {
+                    out.write(bytes[i]);
+                    out.write(hex1);
+                    out.write(hex2);
                 }
+                i = i + 2;
             } else {
                 out.write(bytes[i]);
             }
