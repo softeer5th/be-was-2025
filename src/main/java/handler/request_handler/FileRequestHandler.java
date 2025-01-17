0a2fafd44605cebec09faeabdde767eb4dfc2571
khw7385 <khw7385@naver.com>
2025-01-17 11:17:06 +0900

    feat: 동적 html 핸들러 매핑

--- src/main/java/handler/request_handler/FileRequestHandler.java
+++ src/main/java/handler/request_handler/FileRequestHandler.java
@@ -1,5 +1,6 @@
-package handler;
+package handler.request_handler;
 
+import handler.mapping.DynamicHtmlHandlerMapping;
 import http.request.HttpRequest;
 import http.response.HttpResponse;
 import http.enums.HttpMethod;
@@ -10,6 +11,8 @@ import util.FileUtil;
 import java.io.File;
 
 public class FileRequestHandler implements RequestHandler{
+    private final DynamicHtmlHandlerMapping dynamicHtmlHandlerMapping = DynamicHtmlHandlerMapping.getInstance();
+
     @Override
     public boolean canHandle(HttpRequest httpRequest) {
         if(httpRequest.getMethod() == HttpMethod.GET && FileUtil.isFileExist(httpRequest.getPath())){
@@ -23,12 +26,16 @@ public class FileRequestHandler implements RequestHandler{
         File file = FileUtil.getFile(httpRequest.getPath());
 
         String extension = FileUtil.extractFileExtension(file.getPath());
-        byte[] data = FileUtil.readFileToByteArray(file);
+        byte[] fileData = FileUtil.readFileToByteArray(file);
+
+        byte[] resultData = dynamicHtmlHandlerMapping.getHandler(httpRequest.getPath())
+                .map(handler -> handler.handle(fileData, httpRequest))
+                .orElse(fileData);
 
         return new HttpResponse.Builder()
                 .httpStatus(HttpStatus.OK)
                 .contentType(MimeType.getMimeType(extension))
-                .body(data)
+                .body(resultData)
                 .build();
     }
 }
