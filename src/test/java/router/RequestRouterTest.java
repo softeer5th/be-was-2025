10b1f1a4ee5df9ef419c18c46c3a263381bd7a53
zoouniak <jueun1025@naver.com>
2025-01-17 11:47:35 +0900

    test : requestRouter test

---
+++ src/test/java/router/RequestRouterTest.java
@@ -0,0 +1,66 @@
+package router;
+
+import exception.ClientErrorException;
+import exception.ErrorCode;
+import handler.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RequestRouterTest {
+    RequestRouter router = new RequestRouter();
+    @Test
+    @DisplayName("path가 /user/로 시작하면 userRequestHandler를 반환한다.")
+    void routeToUserRequestHandler() {
+        String path = "/user/create";
+        final Handler handler = router.route(path);
+        Assertions.assertThat(handler)
+                .isInstanceOf(UserRequestHandler.class);
+    }
+
+    @Test
+    @DisplayName("path가 /index.html이면 DynamicHomeHandler를 반환한다.")
+    void routeToDynamicHomeHandler() {
+        String path = "/index.html";
+        final Handler handler = router.route(path);
+        Assertions.assertThat(handler)
+                .isInstanceOf(DynamicHomeHandler.class);
+    }
+
+    @Test
+    @DisplayName("path가 /mypage/index.html이면 MyPageHandler를 반환한다.")
+    void routeToMyPageHandler() {
+        String path = "/mypage/index.html";
+        final Handler handler = router.route(path);
+        Assertions.assertThat(handler)
+                .isInstanceOf(MyPageHandler.class);
+    }
+
+    @Test
+    @DisplayName("path가 파일 확장자로 끝나면 staticFileHandler로 라우팅한다.")
+    void routeToStaticFileHandler() {
+        String path = "/another/index.html";
+        final Handler handler = router.route(path);
+        Assertions.assertThat(handler)
+                .isInstanceOf(StaticFileHandler.class);
+
+    }
+
+    @Test
+    @DisplayName("path가 /이면 HomeHandler로 라우팅한다")
+    void routeToHomeHandler() {
+        String path = "/";
+        final Handler handler = router.route(path);
+        Assertions.assertThat(handler)
+                .isInstanceOf(HomeHandler.class);
+    }
+
+    @Test
+    @DisplayName("라우터에 등록되지 않은 path로 요청이 에러가 발생한다.")
+    void route_invalidPath() {
+        String path = "/invalid";
+        Assertions.assertThatThrownBy(()->router.route(path))
+                .isInstanceOf(ClientErrorException.class)
+                .hasMessage(ErrorCode.NOT_ALLOWED_PATH.getMessage());
+    }
+}
