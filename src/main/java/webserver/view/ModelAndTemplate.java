70b1f7d5dc74b214777b4e951d7a95aa6516818a
vvsos1 <vvsos10@naver.com>
2025-01-18 22:47:01 +0900

    feat. add error message display functionality in view

--- src/main/java/webserver/view/ModelAndTemplate.java
+++ src/main/java/webserver/view/ModelAndTemplate.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 // template 이름과 렌더링할 데이터를 가지고 있는 클래스
 public class ModelAndTemplate {
+    private static final String ERROR_ATTRIBUTE_NAME = "error";
     private final String templateName;
     // template 을 렌더링할 때 필요한 데이터
     private final Map<String, Object> model;
@@ -24,6 +25,10 @@ public class ModelAndTemplate {
 
     public void addAttribute(String key, Object value) {
         model.put(key, value);
+    }
+
+    public void setError(String errorMessage) {
+        model.put(ERROR_ATTRIBUTE_NAME, errorMessage);
     }
 
     public Map<String, Object> getModel() {
