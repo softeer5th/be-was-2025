46bbf0955a551267e5b2939a11f6609902d5192a
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-18 11:43:25 +0900

    feat: 마이 페이지 및 비밀번호 수정 기능 추가

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,6 +2,7 @@ package controller;
 
 import db.Database;
 import model.User;
+import tag.HeaderMenu;
 import wasframework.HttpSession;
 import wasframework.Mapping;
 import webserver.httpserver.HttpMethod;
@@ -16,12 +17,14 @@ import java.io.IOException;
 import java.util.UUID;
 
 import static utils.FileUtils.getFile;
+import static utils.FileUtils.getFileAsString;
 import static wasframework.HttpSession.SESSION_ID;
 
 public class UserController {
 
     /**
      * 로그인 페이지를 제공하는 핸들러.
+     *
      * @param response 정상적으로 생성된 response 객체만 들어옴.
      * @throws IOException
      */
@@ -68,6 +71,7 @@ public class UserController {
 
     /**
      * 로그인 실패 창 서빙 핸들러
+     *
      * @param response 정상적으로 생성된 response 객체
      * @throws IOException
      */
@@ -83,22 +87,72 @@ public class UserController {
     /**
      * 로그아웃 요청 처리 핸들러.
      * 로그인 된 회원의 경우, 세션 저장소에서 해당 회원의 세션을 지우고, 해당 회원의 쿠키를 만료시킴.
-     * @param request 정상적으로 파싱된 request 객체
+     *
+     * @param request  정상적으로 파싱된 request 객체
      * @param response 정상적으로 생성된 response 객체
      */
     @Mapping(path = "/logout", method = HttpMethod.POST)
     public void logout(HttpRequest request, HttpResponse response) {
         Cookie cookie = request.getCookie();
         String sessionId = cookie.getCookie(SESSION_ID);
-        if (sessionId != null){
+        if (sessionId != null) {
             String userId = HttpSession.get(sessionId);
-            if(userId != null){
+            if (userId != null) {
                 HttpSession.remove(sessionId);
                 SetCookie newCookie = new SetCookie();
                 newCookie.setMaxAge(0);
                 response.setCookie(newCookie);
             }
         }
+        response.setLocation("/");
+    }
+
+    @Mapping(path = "/mypage", method = HttpMethod.GET)
+    public void mypage(HttpRequest request, HttpResponse response) throws IOException {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+
+        Cookie cookie = request.getCookie();
+        String sessionId = cookie.getCookie(SESSION_ID);
+
+        String userId = HttpSession.get(sessionId);
+        if (sessionId == null || userId == null) {
+            response.setLocation("/login");
+            return;
+        }
+
+        File file = new File("src/main/resources/static/mypage/index.html");
+        byte[] readFile = getFile(file);
+        response.setBody(readFile);
+    }
+
+    @Mapping(path = "/user/edit", method = HttpMethod.POST)
+    public void changePassword(HttpRequest request, HttpResponse response) throws IOException {
+
+        Cookie cookie = request.getCookie();
+        String sessionId = cookie.getCookie(SESSION_ID);
+
+        String userId = HttpSession.get(sessionId);
+
+        String inputUsername = request.getParameter("username");
+
+        if (userId == null || request.getParameter("password") == null) {
+            response.setLocation("/error/401.html");
+            response.setStatusCode(StatusCode.UNAUTHORIZED);
+            return;
+        }
+
+        User userById = Database.findUserById(userId);
+        if (sessionId == null || !userById.getName().equals(inputUsername)) {
+            response.setLocation("/error/401.html");
+            response.setStatusCode(StatusCode.UNAUTHORIZED);
+            return;
+        }
+        if (!request.getParameter("password").equals(request.getParameter("passwordRewrite"))) {
+            response.setLocation("/mypage");
+            return;
+        }
+
+        userById.changePassword(request.getParameter("password"));
         response.setLocation("/");
     }
 }
