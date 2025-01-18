3a061de68fe4abf4cad14cddf9c20cf80ab56c35
vvsos1 <vvsos10@naver.com>
2025-01-18 22:47:17 +0900

    feat. add error message of login fail

--- src/main/java/handler/LoginHandler.java
+++ src/main/java/handler/LoginHandler.java
@@ -2,11 +2,13 @@ package handler;
 
 import db.Database;
 import model.User;
+import webserver.enums.HttpStatusCode;
 import webserver.exception.BadRequest;
 import webserver.handler.HttpHandler;
 import webserver.request.HttpRequest;
 import webserver.response.HttpResponse;
 import webserver.session.HttpSession;
+import webserver.view.ModelAndTemplate;
 
 import java.util.Map;
 import java.util.Optional;
@@ -14,6 +16,7 @@ import java.util.Optional;
 import static webserver.enums.PageMappingPath.INDEX;
 
 public class LoginHandler implements HttpHandler {
+    private static final String TEMPLATE_NAME = "/login/index.html";
     private final Database database;
 
     public LoginHandler(Database database) {
@@ -22,7 +25,7 @@ public class LoginHandler implements HttpHandler {
 
     @Override
     public HttpResponse handleGet(HttpRequest request) {
-        return HttpResponse.render("/login/index.html");
+        return HttpResponse.render(TEMPLATE_NAME);
     }
 
     @Override
@@ -33,10 +36,18 @@ public class LoginHandler implements HttpHandler {
         Optional<User> user = database.findUserById(userId);
         if (user.filter(u ->
                 u.isPasswordCorrect(password)).isEmpty()) {
-            return HttpResponse.redirect("/user/login_failed.html");
+            return renderLoginPageWithErrorMessage();
         }
         HttpSession session = request.getSession();
         session.set(HttpSession.USER_KEY, user.get());
         return HttpResponse.redirect(INDEX.path);
+    }
+
+    private HttpResponse renderLoginPageWithErrorMessage() {
+        HttpResponse response = new HttpResponse(HttpStatusCode.UNAUTHORIZED);
+        ModelAndTemplate modelAndTemplate = new ModelAndTemplate(TEMPLATE_NAME);
+        modelAndTemplate.setError("아이디 또는 비밀번호가 틀립니다.");
+        response.renderTemplate(modelAndTemplate);
+        return response;
     }
 }
