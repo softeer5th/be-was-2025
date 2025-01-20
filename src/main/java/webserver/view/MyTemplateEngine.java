ee0b9a91390a725fad26403418fa06de2cdb29e7
vvsos1 <vvsos10@naver.com>
2025-01-20 14:50:06 +0900

    feat. add compare operator of <my-if>

--- src/main/java/webserver/view/MyTemplateEngine.java
+++ src/main/java/webserver/view/MyTemplateEngine.java
@@ -1,5 +1,8 @@
 package webserver.view;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
@@ -10,14 +13,13 @@ import java.util.regex.Pattern;
 // 각 커스텀 태그를 처리하는 TagRenderer를 등록하여 사용한다.
 // 템플릿 문자열을 파싱하여 커스텀 태그를 찾고, 각 태그를 렌더링하여 최종 결과를 반환한다.
 public class MyTemplateEngine implements TemplateEngine {
+
     // my- 로 시작하는 여는 태그 및 닫는 태그를 찾기 위한 정규식
-    private static final Pattern tagPattern = Pattern.compile("<(/?my-[a-z]+)(( *([a-z]+)=\"([a-z0-9.&|!/]+)\")*)>");
+    private static final Pattern TAG_PATTERN = Pattern.compile("<(/?my-[a-z]+)((?: *\\w+=\"[^\"]+\")*)>");
+    private static final Pattern ATTRIBUTE_PATTERN = Pattern.compile(" *(\\w+)=\"([^\"]+)\"");
     private static final String TAG_NAME_PREFIX = "my-";
-    private static final String ATTRIBUTE_DELIMITER = " ";
-    private static final String KEY_VALUE_DELIMITER = "=";
     private static final String CLOSE_TAG_NAME_PREFIX = "/";
-    private static final String CLOSE_TAG_PREFIX = "</";
-    private static final String OPEN_TAG_SUFFIX = ">";
+    private static final Logger log = LoggerFactory.getLogger(MyTemplateEngine.class);
 
     private final Map<String, TagRenderer> tagHandlers = new ConcurrentHashMap<>();
 
@@ -41,48 +43,46 @@ public class MyTemplateEngine implements TemplateEngine {
         for (int i = 0; i < matchingResults.size(); i++) {
             TagMatchingResult result = matchingResults.get(i);
             TagRenderer tagRenderer = tagHandlers.get(result.tagName);
-            String childrenTemplate = extractChildrenTemplate(template, result);
+            String childrenTemplate = result.childTemplate;
 
             // plain html 부분을 렌더링 결과에 추가
             int start = i == 0 ? 0 : matchingResults.get(i - 1).lastTagEnd;
             rendered.append(template, start, result.firstTagStart);
 
             // 태그를 렌더링하여 렌더링 결과에 추가
-            rendered.append(tagRenderer.handle(model, result.tagAttributes, childrenTemplate));
+            rendered.append(tagRenderer.handle(model, result.tagAttributes, childrenTemplate).strip());
         }
         // 남아있는 plain html 부분을 렌더링 결과에 추가
         rendered.append(template, matchingResults.get(matchingResults.size() - 1).lastTagEnd, template.length());
-        return rendered.toString();
+        return rendered.toString().strip();
     }
 
-    // 매칭된 태그 쌍의 자식 요소를 문자열로 추출
-    private String extractChildrenTemplate(String template, TagMatchingResult result) {
-        int openTagEnd = template.indexOf(OPEN_TAG_SUFFIX, result.firstTagStart) + 1;
-        int closeTagStart = template.lastIndexOf(CLOSE_TAG_PREFIX + result.tagName, result.lastTagEnd);
-        return template.substring(openTagEnd, closeTagStart).strip();
-    }
 
     // 매칭되는 태그 쌍 찾기
     private List<TagMatchingResult> matchTag(String template) {
         List<TagMatchingResult> matchingResults = new ArrayList<>();
-        Matcher matcher = tagPattern.matcher(template);
+        Matcher matcher = TAG_PATTERN.matcher(template);
         // 태그 쌍을 찾기 위한 스택
         Stack<String> openTagStack = new Stack<>();
 
         boolean isCurrentTagFound = false;
         int currentTagStart = -1;
         int currentTagEnd = -1;
+        int currentOpenTagEnd = -1;
+        int currentCloseTagStart = -1;
 
         String currentTagName = null;
         Map<String, String> currentTagAttributes = null;
 
         while (matcher.find()) {
+            log.debug("find tag: {}", matcher.group(0));
             String tagName = matcher.group(1);
             if (!isCurrentTagFound) {
                 // fist open tag
                 currentTagName = tagName;
                 currentTagStart = matcher.start(0);
                 currentTagAttributes = parseAttributes(matcher.group(2));
+                currentOpenTagEnd = matcher.end(0);
                 isCurrentTagFound = true;
             }
             if (tagName.startsWith(CLOSE_TAG_NAME_PREFIX)) {
@@ -94,7 +94,9 @@ public class MyTemplateEngine implements TemplateEngine {
                 if (openTagStack.isEmpty()) {
                     // last close tag
                     currentTagEnd = matcher.end(0);
-                    matchingResults.add(new TagMatchingResult(currentTagStart, currentTagEnd, currentTagName, currentTagAttributes));
+                    currentCloseTagStart = matcher.start(0);
+                    String childTemplate = template.substring(currentOpenTagEnd, currentCloseTagStart);
+                    matchingResults.add(new TagMatchingResult(currentTagStart, currentTagEnd, currentTagName, currentTagAttributes, childTemplate));
                     isCurrentTagFound = false;
                 }
             } else {
@@ -107,13 +109,12 @@ public class MyTemplateEngine implements TemplateEngine {
     // 태그의 속성을 파싱하여 Map으로 반환
     private Map<String, String> parseAttributes(String attributes) {
         Map<String, String> result = new HashMap<>();
-        String[] split = attributes.split(ATTRIBUTE_DELIMITER);
-        for (String s : split) {
-            if (s.isBlank()) {
-                continue;
-            }
-            String[] keyValue = s.split(KEY_VALUE_DELIMITER);
-            result.put(keyValue[0], keyValue[1].substring(1, keyValue[1].length() - 1));
+        Matcher matcher = ATTRIBUTE_PATTERN.matcher(attributes);
+        while (matcher.find()) {
+            String key = matcher.group(1);
+            String value = matcher.group(2);
+            log.debug("key: {}, value: {}", key, value);
+            result.put(key, value);
         }
         return result;
     }
@@ -123,7 +124,8 @@ public class MyTemplateEngine implements TemplateEngine {
             int firstTagStart, // 여는 태그 시작 인덱스
             int lastTagEnd,  // 닫는 태그 끝 인덱스
             String tagName, // 태그 이름
-            Map<String, String> tagAttributes // 태그 속성
+            Map<String, String> tagAttributes, // 태그 속성
+            String childTemplate
     ) {
 
     }
--- src/main/java/webserver/view/renderer/IfTagRenderer.java
+++ src/main/java/webserver/view/renderer/IfTagRenderer.java
@@ -1,10 +1,14 @@
 package webserver.view.renderer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.ReflectionUtil;
+import webserver.exception.InternalServerError;
 import webserver.view.TagRenderer;
 import webserver.view.TemplateEngine;
 
 import java.util.Map;
+import java.util.Objects;
 
 // 조건문이 참이면 childTemplate을 렌더링하고, 거짓이면 빈 문자열을 반환하는 TagRenderer
 // 지원하는 조건문 형식은 다음과 같다.
@@ -16,9 +20,11 @@ import java.util.Map;
 public class IfTagRenderer extends TagRenderer {
     public static final String IF_TAG_NAME = "my-if";
     public static final String CONDITION_ATTRIBUTE_NAME = "condition";
-    private static final String BINARY_OPERATOR_PATTERN = "(&&)|(\\|\\|)";
+    private static final String LOGICAL_BINARY_OPERATOR_PATTERN = "(&&)|(\\|\\|)";
+    private static final String COMPARE_BINARY_OPERATOR_PATTERN = "(>=)|(<=)|(>)|(<)|(==)|(!=)";
     private static final String NOT_OPERATOR = "!";
     private static final String EMPTY_STRING = "";
+    private static final Logger log = LoggerFactory.getLogger(IfTagRenderer.class);
     private TemplateEngine engine;
 
     public IfTagRenderer() {
@@ -47,9 +53,29 @@ public class IfTagRenderer extends TagRenderer {
     private boolean isConditionTrue(Map<String, Object> model, String condition) {
         condition = condition.strip();
         // condition을 맨 앞에 오는 이항 연산자 기준으로 나누기
-        String[] tokens = condition.split(BINARY_OPERATOR_PATTERN, 2);
+        String[] tokens = condition.split(LOGICAL_BINARY_OPERATOR_PATTERN, 2);
         if (tokens.length == 1) {
             // tokens[0] == condition 인 상황
+
+            // condition에 비교 연산자가 포함된 경우
+            String[] compareTokens = condition.split(COMPARE_BINARY_OPERATOR_PATTERN);
+            if (compareTokens.length > 2)
+                throw new InternalServerError("비교 연산자가 잘못 사용되었습니다.");
+            if (compareTokens.length == 2) {
+                String left = compareTokens[0].strip();
+                String right = compareTokens[1].strip();
+                String operator = condition.substring(left.length(), condition.length() - right.length()).strip();
+                log.debug("compare operator. left:{}, operator:{}, right:{}", left, operator, right);
+                return switch (operator) {
+                    case ">=" -> parseInt(model, left) >= parseInt(model, right);
+                    case "<=" -> parseInt(model, left) <= parseInt(model, right);
+                    case ">" -> parseInt(model, left) > parseInt(model, right);
+                    case "<" -> parseInt(model, left) < parseInt(model, right);
+                    case "==" -> Objects.equals(parseInt(model, left), parseInt(model, right));
+                    case "!=" -> !Objects.equals(parseInt(model, left), parseInt(model, right));
+                    default -> throw new InternalServerError("비교 연산자가 잘못 사용되었습니다. 연산자:" + operator);
+                };
+            }
 
             // condition에 not 연산자가 포함된 경우
             if (condition.startsWith(NOT_OPERATOR)) {
@@ -77,4 +103,18 @@ public class IfTagRenderer extends TagRenderer {
             };
         }
     }
+
+    private Integer parseInt(Map<String, Object> model, String condition) {
+        // 정수로 변환 가능한 경우 Optional로 감싸서 반환
+        try {
+            return Integer.parseInt(condition);
+        } catch (NumberFormatException ignored) {
+        }
+        // 정수로 변환할 수 없는 경우 객체 탐색 경로로 간주
+        return ReflectionUtil.recursiveCallGetter(model, condition)
+                .filter(o -> o instanceof Integer)
+                .map(o -> (Integer) o)
+                .orElseThrow(() -> new InternalServerError("정수로 변환할 수 없는 객체 탐색 경로입니다."));
+    }
+
 }
