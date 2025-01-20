7830624900018a878cd290affa4ab580d6fe9eeb
codrin2 <hssang127@gmail.com>
2025-01-20 15:59:00 +0900

    feat(User): User 모델에 프로필 이미지 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,40 +5,42 @@ public class User {
     private String password;
     private String name;
     private String email;
+    // 추가
+    private byte[] profileImage;
 
-    public User(String userId, String password, String name, String email) {
+    public User(String userId, String password, String name, String email, byte[] profileImage) {
         this.userId = userId;
         this.password = password;
         this.name = name;
         this.email = email;
+        this.profileImage = profileImage;
     }
 
     public String getUserId() {
         return userId;
     }
-
     public String getPassword() {
         return password;
     }
-
     public String getName() {
         return name;
     }
-
     public String getEmail() {
         return email;
+    }
+
+    public byte[] getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(byte[] profileImage) {
+        this.profileImage = profileImage;
     }
 
     public void setName(String newName) {
         this.name = newName;
     }
-
     public void setPassword(String newPassword) {
         this.password = newPassword;
-    }
-
-    @Override
-    public String toString() {
-        return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
     }
 }
