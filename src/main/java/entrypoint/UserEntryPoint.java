f455bd6d2f3f8662c83232720d42a639dd4c953e
ji-woong-song <thdwldnd7@naver.com>
2025-01-20 14:55:35 +0900

    feat: 로그인 요청처리 메서드 추가

--- src/main/java/entrypoint/UserEntryPoint.java
+++ src/main/java/entrypoint/UserEntryPoint.java
@@ -7,6 +7,10 @@ import webserver.annotation.RequestMapping;
 import webserver.enumeration.HTTPMethod;
 import webserver.exception.HTTPException;
 import webserver.message.record.ResponseData;
+import webserver.message.record.SetCookieRecord;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class UserEntryPoint {
     @RequestMapping(path = "/user/create", method = HTTPMethod.POST)
@@ -22,5 +26,25 @@ public class UserEntryPoint {
         User user = new User(userId, nickname, password, "mock@mock.com");
         Database.addUser(user);
         return ResponseData.redirect("/index.html");
+    }
+
+    @RequestMapping(path= "/login", method = HTTPMethod.POST)
+    public ResponseData<String> login(
+            @Body(key="userId") String userId,
+            @Body(key="password") String password
+    ) {
+        User user = Database.findUserById(userId);
+        if (user == null || !user.getPassword().equals(password)) {
+            return ResponseData.redirect("/user/login_failed.html");
+        }
+        String sessionId = UUID.randomUUID().toString();
+        SetCookieRecord loginCookie = new SetCookieRecord(
+            "SID",
+            sessionId,
+            LocalDateTime.of(2025,12,31,0,0)
+        );
+        return new ResponseData.ResponseDataBuilder<String>()
+                .setCookies(loginCookie)
+                .redirect("/index.html");
     }
 }
