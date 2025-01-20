c8284eecd670c39a3d30394579b8dd34da37341f
uri010 <dbfl0461@gmail.com>
2025-01-20 18:45:37 +0900

    [Test] FileRequestHandlerTest 에러 수정

--- src/test/java/handler/FileRequestHandlerTest.java
+++ src/test/java/handler/FileRequestHandlerTest.java
@@ -13,16 +13,32 @@ import http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 public class FileRequestHandlerTest {
 
+    private static final HttpMethod HTTP_METHOD = HttpMethod.GET;
     private static final String VALID_FILE_PATH = "/index.html";
     private static final String INVALID_FILE_PATH = "/invalid.html";
 
-    /*@Test
+
+    private HttpRequestInfo createHttpRequest(HttpMethod method, String path) throws IOException {
+        String rawRequest =
+                method + " " + path + " HTTP/1.1\r\n" +
+                        "Host: localhost";
+
+        InputStream inputStream = new ByteArrayInputStream(rawRequest.getBytes(StandardCharsets.UTF_8));
+        return new HttpRequestInfo(inputStream);
+    }
+
+    @Test
     @DisplayName("정적파일 로드 성공")
-    void testHandleWithExistingFile() {
+    void testHandleWithExistingFile() throws IOException {
         FileRequestHandler handler = new FileRequestHandler();
-        HttpRequestInfo request = new HttpRequestInfo(HttpMethod.GET, VALID_FILE_PATH, null);
+        HttpRequestInfo request = createHttpRequest(HTTP_METHOD, VALID_FILE_PATH);
 
         HttpResponse response = handler.handle(request);
 
@@ -32,13 +48,13 @@ public class FileRequestHandlerTest {
 
     @Test
     @DisplayName("정적파일 로드 실패")
-    void testHandleWithFileNotFound() {
+    void testHandleWithFileNotFound() throws IOException {
         FileRequestHandler handler = new FileRequestHandler();
-        HttpRequestInfo request = new HttpRequestInfo(HttpMethod.GET, INVALID_FILE_PATH, null);
+        HttpRequestInfo request = createHttpRequest(HTTP_METHOD, INVALID_FILE_PATH);
 
         BaseException baseException = assertThrows(BaseException.class,
-            () -> handler.handle(request));
+                () -> handler.handle(request));
         assertEquals(baseException.getMessage(), FileErrorCode.FILE_NOT_FOUND.getMessage());
     }
-*/
+
 }
