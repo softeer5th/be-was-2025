f3e025d8d5d49ed9c15c8603a7839194068282ff
vvsos1 <vvsos10@naver.com>
2025-01-20 14:49:24 +0900

    feat. add iteration index attribute of <my-foreach>

--- src/main/java/webserver/view/renderer/ForeachTagRenderer.java
+++ src/main/java/webserver/view/renderer/ForeachTagRenderer.java
@@ -15,8 +15,12 @@ import static util.ReflectionUtil.recursiveCallGetter;
 // 각 렌더링 시 item을 model에 추가하여 childTemplate에서 item을 사용할 수 있게 한다.
 public class ForeachTagRenderer extends TagRenderer {
     public static final String DEFAULT_TAG_NAME = "my-foreach";
+    // items: 반복할 대상
     public static final String ITEMS_ATTRIBUTE_NAME = "items";
+    // item: 각 반복 시 사용할 이름
     public static final String ITEM_ATTRIBUTE_NAME = "item";
+    // index: 각 반복 시 index를 사용할 이름
+    public static final String INDEX_ATTRIBUTE_NAME = "index";
 
     private TemplateEngine engine;
 
@@ -36,6 +40,7 @@ public class ForeachTagRenderer extends TagRenderer {
     public String handle(Map<String, Object> model, Map<String, String> tagAttributes, String childrenTemplate) {
         String itemsAttribute = tagAttributes.get(ITEMS_ATTRIBUTE_NAME);
         String itemAttribute = tagAttributes.get(ITEM_ATTRIBUTE_NAME);
+        String indexAttribute = tagAttributes.get(INDEX_ATTRIBUTE_NAME);
         if (model.containsKey(itemAttribute))
             throw new InternalServerError(ITEM_ATTRIBUTE_NAME + " attribute가 이미 모델에 있습니다.");
 
@@ -45,12 +50,16 @@ public class ForeachTagRenderer extends TagRenderer {
             throw new InternalServerError(ITEMS_ATTRIBUTE_NAME + " attribute가 모델에 없습니다.");
         Iterator<?> iterator = resolveIterator(itemsObject.get());
 
-        while (iterator.hasNext()) {
+        for (int i = 0; iterator.hasNext(); i++) {
             Object item = iterator.next();
             model.put(itemAttribute, item);
+            if (indexAttribute != null)
+                model.put(indexAttribute, i);
             result.append(engine.render(childrenTemplate, model));
-            model.remove(itemAttribute);
         }
+        model.remove(itemAttribute);
+        if (indexAttribute != null)
+            model.remove(indexAttribute);
         return result.toString();
     }
 
