21c9938d51b6fdc6539f0f6b2789b2a370dad62d
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-21 18:22:54 +0900

    feat: boardDao.save(board) 가 ID를 포함한 새 객체를 생성하고 반환하도록 변경

--- src/main/java/db/BoardDao.java
+++ src/main/java/db/BoardDao.java
@@ -24,7 +24,11 @@ public enum BoardDao {
     BoardDao(long initValue) {
         this.boardSize = new AtomicLong(initValue);
     }
-
+    /**
+     * id와 매칭되는 게시글을 찾아주는 메소드
+     * @param boardId 찾고자 하는 board ID
+     * @return Optional 로 감싼 board 객체 반환. 해당 ID가 존재하지 않을 경우, 빈 Optional 객체 반환
+     */
     public Optional<Board> findById(Long boardId) {
         String sql = "select * from boards where board_id = ?";
         Connection con = null;
@@ -50,7 +54,11 @@ public enum BoardDao {
         }
         return Optional.empty();
     }
-
+    /**
+     * 새 게시글을 저장하는 메소드
+     * @param board id값이 없는 새로 저장할 board 객체
+     * @return
+     */
     public Optional<Board> save(Board board){
         String sql = "insert into boards(board_id, contents, writer, image_path) values (?, ?, ?, ?)";
         Connection con = null;
@@ -58,12 +66,14 @@ public enum BoardDao {
         try{
             con = getConnection();
             pstmt = con.prepareStatement(sql);
-            pstmt.setLong(1, boardSize.incrementAndGet());
+            long boardId = boardSize.incrementAndGet();
+            pstmt.setLong(1, boardId);
             pstmt.setString(2, board.getContents());
             pstmt.setString(3, board.getWriter());
             pstmt.setString(4, board.getImagePath());
             pstmt.executeUpdate();
-            return Optional.of(board);
+            Board newBoard = new Board(boardId, board.getWriter(), board.getContents(), board.getImagePath());
+            return Optional.of(newBoard);
         } catch (SQLException e) {
             log.error("save 예외: ", e);
         }finally {
