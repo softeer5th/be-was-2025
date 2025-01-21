e3a97a3f4fcf6b2871b26e90c05bc21f10178914
mikekks <thdalsrb79@naver.com>
2025-01-21 17:30:25 +0900

    [step7] add: h2 db 관련 환경 세팅

---
+++ src/main/java/db/DataSource.java
@@ -0,0 +1,49 @@
+package db;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DataSource {
+	private static DataSource INSTANCE = new DataSource();
+	private Properties properties;
+
+	private DataSource() {
+		Properties properties = new Properties();
+
+		try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
+			if (input == null) {
+				throw new RuntimeException("Properties file not found in classpath");
+			}
+
+			properties.load(input);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		this.properties = properties;
+	}
+
+	// 싱글톤
+	public static DataSource getInstance() {
+		if (INSTANCE == null) {
+			synchronized (DataSource.class) {
+				if (INSTANCE == null) {
+					INSTANCE = new DataSource();
+				}
+			}
+		}
+
+		return INSTANCE;
+	}
+
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(
+			properties.getProperty("db.url"),
+			properties.getProperty("db.user"),
+			properties.getProperty("db.password"));
+	}
+}
