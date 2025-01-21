605a12b98fa78bb302d779b4d31de5ccfaebd9cf
sunohkim <sunoh1222@gmail.com>
2025-01-21 15:18:51 +0900

    style : 에러 로그 출력 양식 수정 refactor된 HTTPExceptions 클래스에 맞게 UserCreateHandler 클래스의 에러 발생 시 로그 출력 내용을 수정했다.

--- src/main/java/webserver/request/service/UserCreateHandler.java
+++ src/main/java/webserver/request/service/UserCreateHandler.java
@@ -26,8 +26,9 @@ public class UserCreateHandler implements RequestProcessor {
         try {
             Map<String, String> headers = requestHeader.getHeaders();
             // 지정된 Content-Type이 아닐 경우
-            if (!headers.get("content-type").equals("application/x-www-form-urlencoded")) {
-                throw new HTTPExceptions.Error415("415 Unsupported Media Type");
+            String contentType = headers.get("content-type");
+            if (!contentType.equals("application/x-www-form-urlencoded")) {
+                throw new HTTPExceptions.Error415("Unsupported Media Type " + contentType);
             }
 
             String[] params = requestBody.getBodyToString().split("&");
@@ -36,17 +37,17 @@ public class UserCreateHandler implements RequestProcessor {
                 String[] keyValue = param.split("=");
                 // 키값에 등호가 있을 경우
                 if (keyValue.length != 2) {
-                    throw new HTTPExceptions.Error400("400 Bad Request: Invalid key");
+                    throw new HTTPExceptions.Error400("Invalid key");
                 }
                 // 키값 중복
                 if (paramMap.containsKey(keyValue[0])) {
-                    throw new HTTPExceptions.Error400("400 Bad Request: Duplicate key");
+                    throw new HTTPExceptions.Error400("Duplicate key");
                 }
                 paramMap.put(keyValue[0], URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8));
             }
             // 잘못된 키값 입력
             if (paramMap.size() != 3) {
-                throw new HTTPExceptions.Error400("400 Bad Request: wrong number of parameters");
+                throw new HTTPExceptions.Error400("wrong number of parameters");
             }
 
             String userId = paramMap.get("id");
@@ -56,7 +57,7 @@ public class UserCreateHandler implements RequestProcessor {
             // 중복된 id를 가진 사용자가 있을 경우
             if (Database.findUserById(userId) != null) {
                 // 409 Conflict
-                throw new HTTPExceptions.Error409("409 Conflict: User already exists");
+                throw new HTTPExceptions.Error409("User already exists");
             }
 
             // User 데이터베이스에 사용자 정보 추가
