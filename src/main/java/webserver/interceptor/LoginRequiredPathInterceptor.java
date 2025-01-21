a29f047d088b2e1b7d7a9db285114a8d9f00530d
vvsos1 <vvsos10@naver.com>
2025-01-21 17:21:02 +0900

    feat. handle paths with path variables in LoginRequiredPathInterceptor

--- src/main/java/webserver/interceptor/LoginRequiredPathInterceptor.java
+++ src/main/java/webserver/interceptor/LoginRequiredPathInterceptor.java
@@ -5,17 +5,22 @@ import webserver.exception.LoginRequired;
 import webserver.request.HttpRequest;
 import webserver.session.HttpSession;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import static enums.PageMappingPath.LOGIN;
 
 // 특정한 페이지로의 로그인 없는 접근을 막는 인터셉터
 public class LoginRequiredPathInterceptor implements HandlerInterceptor {
     private static final String REDIRECT_URL = LOGIN.path;
-    private final Set<String> loginRequiredPaths;
+    private final List<Pattern> loginRequiredPaths;
 
     public LoginRequiredPathInterceptor(String... paths) {
-        this.loginRequiredPaths = Set.of(paths);
+        loginRequiredPaths = new ArrayList<>();
+        for (String path : paths) {
+            loginRequiredPaths.add(Pattern.compile(path.replaceAll("\\{[^/]+}", "[^/]+")));
+        }
     }
 
     @Override
@@ -23,7 +28,7 @@ public class LoginRequiredPathInterceptor implements HandlerInterceptor {
         Object loginUser = request.getSession().get(HttpSession.USER_KEY);
         String requestPath = request.getRequestTarget().getPath();
         // 로그인이 필요한 페이지에 로그인하지 않은 경우
-        if (loginUser == null && loginRequiredPaths.contains(requestPath)) {
+        if (loginUser == null && loginRequiredPaths.stream().anyMatch(pattern -> pattern.matcher(requestPath).matches())) {
             throw new LoginRequired(REDIRECT_URL);
         }
         return request;
