cc998c4896e89c3fc9f429ebfacee2b4e940e8a3
kwon204 <dafeg3@naver.com>
2025-01-21 17:01:41 +0900

    remove: 필요없는 주석 삭제

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -5,8 +5,6 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import db.ArticleStore;
-import db.CommentStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,11 +21,9 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
                 executor.execute(new RequestHandler(connection));
