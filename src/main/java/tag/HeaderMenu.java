aed1b4f5f4899036a684e6a32c3c347cde837e7b
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-21 16:51:09 +0900

    feat: rendering 기능 HeaderMenu 태그 담당하는 Enum 에 부여

--- src/main/java/tag/HeaderMenu.java
+++ src/main/java/tag/HeaderMenu.java
@@ -30,4 +30,22 @@ public enum HeaderMenu {
     public String getTag() {
         return tag;
     }
+
+    public static String getWelcomeTag(String userId) {
+        return "<li class=\"header__menu__item\">\n" +
+                "<a class=\"btn btn_ghost btn_size_s\" href=\"/mypage\">" +
+                userId + "님, 환영합니다!</a> </li>";
+    }
+
+    public static String renderHeaderMenu(String sessionId, String userId, String readFile){
+        String usernameTag = (sessionId != null && userId != null) ?
+                getWelcomeTag(userId) : "";
+        String loginWriteTag = (sessionId != null && userId != null) ? HeaderMenu.WRITE.getTag() : HeaderMenu.LOGIN.getTag();
+        String signupLogoutTag = (sessionId != null && userId != null) ? HeaderMenu.LOGOUT.getTag() : HeaderMenu.SIGNUP.getTag();
+
+
+        return readFile.replace("${username}", usernameTag)
+                .replace("${loginWrite}", loginWriteTag)
+                .replace("${signupLogout}", signupLogoutTag);
+    }
 }
