1596ba5aff00eca838cb53c43e1fe7ababfa6ee3
ji-woong-song <thdwldnd7@naver.com>
2025-01-21 18:25:57 +0900

    fix: 잘못된 url 수정

--- src/main/java/webserver/writer/html/template/IndexPageWriter.java
+++ src/main/java/webserver/writer/html/template/IndexPageWriter.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 import java.util.Optional;
 
 public class IndexPageWriter {
-    private static final String TEMPLATE_NAME = "/index.html";
+    private static final String TEMPLATE_NAME = "/static/index.html";
     private static String postAccountName(Optional<String> nickname) {
          HTMLElement element = Builder.tag(HTMLTag.P)
                 .className("post__account_nickname")
@@ -27,11 +27,12 @@ public class IndexPageWriter {
                 InputStream in = IndexPageWriter.class.getResourceAsStream(TEMPLATE_NAME);
                 TemplateHTMLReader reader = new TemplateHTMLReader(in)
         ) {
-            // TODO:: ${:key} key를 가지고 자동으로 뿌려주는 함수와 매칭
             content.append(reader.readUntil('$'));
             content.append("\n");
+            String key = reader.readBraceValue();
             content.append(postAccountName(username));
             content.append(reader.readUntil('$'));
+            System.out.println(content);
             return content.toString();
         } catch (IOException e) {
             throw new HTTPException.Builder()
