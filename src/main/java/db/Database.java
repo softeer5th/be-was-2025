3442e5818d3bae2c6b2c839593b137cb963f2ea0
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-01-21 16:00:46 +0900

    deprecate: 예전 DB Deprecate

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Deprecated
 public class Database {
     private static Map<String, User> users = new ConcurrentHashMap<>();
 
