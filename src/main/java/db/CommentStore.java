4bd8f46377c70c127b9994ee0b51a3fa1436e023
kwon204 <dafeg3@naver.com>
2025-01-21 17:00:42 +0900

    remove: 사용하지 않는 코드 삭제
    db가 비워진 경우를 처리했기 때문에 db 초기화 코드 삭제

--- src/main/java/db/CommentStore.java
+++ src/main/java/db/CommentStore.java
@@ -24,17 +24,4 @@ public class CommentStore {
     public static List<Comment> findAll() {
         return comments.values().stream().sorted(Comparator.comparing(Comment::getCommentId).reversed()).toList();
     }
-
-    public static void initDb() {
-
-        User user = new User("0", "1", "account", null);
-        Article article = ArticleStore.findArticleById("0").orElseThrow(() -> new RuntimeException());
-        Comment comment1 = new Comment("test comment1", user, article);
-        Comment comment2 = new Comment("test comment2", user, article);
-        CommentStore.addComment(comment1);
-        CommentStore.addComment(comment2);
-        article.getComments().add(comment1);
-        article.getComments().add(comment2);
-        ArticleStore.addArticle(article);
-    }
 }
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -23,8 +23,6 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
-//        initDb();
-
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -36,10 +34,5 @@ public class WebServer {
             }
             executor.shutdown();
         }
-    }
-
-    private static void initDb() {
-        ArticleStore.initDb();
-        CommentStore.initDb();
     }
 }
