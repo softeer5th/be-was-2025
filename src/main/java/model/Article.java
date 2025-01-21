b3d317aec831a1051ee9505da94ae56e67dedac1
songhyeonseong <shsum7@gmail.com>
2025-01-21 15:35:40 +0900

    feat : auto increasment를 사용하기 위해 id를 String에서 Long으로 변경

--- src/main/java/model/Article.java
+++ src/main/java/model/Article.java
@@ -1,25 +1,25 @@
 package model;
 
 public class Article {
-    private String id;
+    private Long id;
     private String userId;
     private String content;
     private String image_path;
 
-    public Article(String id, String userId, String content, String image_path) {
+    public Article(Long id, String userId, String content, String image_path) {
         this.id = id;
         this.userId = userId;
         this.content = content;
         this.image_path = image_path;
     }
 
-    public Article(String id, String userId, String content) {
+    public Article(String userId, String content) {
         this.id = id;
         this.userId = userId;
         this.content = content;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
