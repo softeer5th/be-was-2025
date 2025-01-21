4eaf8def0f093dc4bca39499225f6298cea6a7ba
kwon204 <dafeg3@naver.com>
2025-01-21 10:55:57 +0900

    feat: 홈 페이지도 등록된 게시글과 페이징이 되도록 기능 추가

--- src/main/java/handler/HomeHandler.java
+++ src/main/java/handler/HomeHandler.java
@@ -1,15 +1,25 @@
 package handler;
 
+import db.ArticleStore;
+import db.Database;
 import http.HttpRequest;
 import http.HttpResponse;
 import http.constant.HttpStatus;
+import model.Article;
+import model.Session;
+import model.User;
+import util.DynamicHtmlEditor;
 import util.FileUtils;
+import util.MimeType;
 import util.SessionUtils;
+import util.exception.UserNotFoundException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
-public class HomeHandler implements Handler{
+public class HomeHandler implements Handler {
+    private static final String DEFAULT_PAGE = "0";
 
     @Override
     public void handle(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
@@ -19,11 +29,30 @@ public class HomeHandler implements Handler{
         }
         String path = httpRequest.getPath().toLowerCase();
 
-
         File file = FileUtils.findFile(path);
+        String content = FileUtils.convertToString(file);
+        String extension = FileUtils.getExtension(file);
+        MimeType mimeType = MimeType.valueOf(extension.toUpperCase());
 
+        String page = httpRequest.getQueries().getOrDefault("page", DEFAULT_PAGE);
+
+        List<Article> articles = ArticleStore.findAll();
+        int articleNum = articles.size();
+        int pageNum = Integer.parseInt(page);
+
+        Article article = articles.get(pageNum);
+
+        int prevNum = Math.max(pageNum - 1, 0);
+        int nextNum = Math.min(pageNum + 1, articleNum - 1);
+
+        content = DynamicHtmlEditor.edit(content, "author", article.getUser().getName());
+        content = DynamicHtmlEditor.edit(content, "content", article.getContent());
+        content = DynamicHtmlEditor.edit(content, "prevPage", String.valueOf(prevNum));
+        content = DynamicHtmlEditor.edit(content, "nextPage", String.valueOf(nextNum));
+
+        byte[] body = content.getBytes();
         httpResponse.writeStatusLine(HttpStatus.OK);
-        httpResponse.writeBody(file);
+        httpResponse.writeBody(body, mimeType.getMimeType());
         httpResponse.send();
     }
 }
