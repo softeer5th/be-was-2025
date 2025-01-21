f2ec0ddae89b726c501d83eeb9394a7927c03030
mikekks <thdalsrb79@naver.com>
2025-01-21 17:40:18 +0900

    [step7] feat: User 데이터베이스 구현
    1. 테이블 생성 메서드 추가
    2. 저장 조회 로직 구현

--- src/main/java/db/UserDatabase.java
+++ src/main/java/db/UserDatabase.java
@@ -2,23 +2,112 @@ package db;
 
 import model.User;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
 
 public class UserDatabase {
-    private static Map<String, User> users = new HashMap<>();
+	private static final DataSource dataSource = DataSource.getInstance();
 
-    public static void addUser(User user) {
-        users.put(user.getUserId(), user);
-    }
+	// 싱글톤 보장
+	private static UserDatabase INSTANCE;
 
-    public static Optional<User> findUserById(String userId) {
-        User orDefault = users.getOrDefault(userId, null);
-        return Optional.ofNullable(orDefault);
-    }
+	// 생성자 private으로 접근 제한
+	private UserDatabase() {
+		createTable();
+	}
 
-    public static User findUserByIdOrThrow(String userId) {
-        return findUserById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
-    }
+	public static UserDatabase getInstance() {
+		if (INSTANCE == null) {
+			synchronized (UserDatabase.class) {
+				if (INSTANCE == null) {
+					INSTANCE = new UserDatabase();
+				}
+			}
+		}
+		return INSTANCE;
+	}
+
+	private static void createTable() {
+		String dropTableQuery = "DROP TABLE IF EXISTS user";
+
+		String createTableQuery = """
+			CREATE TABLE IF NOT EXISTS user (
+			    user_id VARCHAR(255) PRIMARY KEY,
+			    password VARCHAR(255),
+			    name VARCHAR(255),
+			    email VARCHAR(255)
+			)
+			""";
+
+		try (Connection connection = dataSource.getConnection();
+			 Statement statement = connection.createStatement()) {
+			statement.execute(dropTableQuery);
+			statement.execute(createTableQuery);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void save(User user) {
+		String query = """
+			INSERT INTO USER (user_id, password, name, email) 
+			VALUES (?, ?, ?, ?);
+  		""";
+
+		try {
+			Connection connection = dataSource.getConnection();
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, user.getUserId());
+			statement.setString(2, user.getPassword());
+			statement.setString(3, user.getName());
+			statement.setString(4, user.getEmail());
+			statement.execute();
+
+			// auto-commit 이 된다.
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	public Optional<User> findUserById(String userId) {
+		String query = """
+			SELECT user_id, password, name, email
+			FROM user
+			WHERE user_id = ?
+  		""";
+
+		try {
+			Connection connection = dataSource.getConnection();
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, userId);
+
+			ResultSet resultSet = statement.executeQuery();
+
+			if (!resultSet.next()) {
+				throw new IllegalArgumentException("N is out of bounds");
+			}
+
+			User user = new User(
+				resultSet.getString("user_id"),
+				resultSet.getString("password"),
+				resultSet.getString("name"),
+				resultSet.getString("email")
+			);
+
+			return Optional.ofNullable(user);
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public User findUserByIdOrThrow(String userId) {
+		return findUserById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+	}
 }
