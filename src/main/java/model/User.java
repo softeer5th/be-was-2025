22521e5e798f7534b91dd9facd9e11966506585d
khw7385 <khw7385@naver.com>
2025-01-22 10:16:47 +0900

    feat: 이메일 속성 추가

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,20 +1,22 @@
 package model;
 
 public class User {
-    private String userId;
+    private Long id;
+    private String loginId;
     private String password;
     private String name;
     private String email;
 
-    public User(String userId, String password, String name, String email) {
-        this.userId = userId;
+    public User(Long id, String loginId, String password, String name, String email) {
+        this.id = id;
+        this.loginId = loginId;
         this.password = password;
         this.name = name;
         this.email = email;
     }
 
     public String getUserId() {
-        return userId;
+        return loginId;
     }
 
     public String getPassword() {
@@ -27,10 +29,5 @@ public class User {
 
     public String getEmail() {
         return email;
-    }
-
-    @Override
-    public String toString() {
-        return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
     }
 }
