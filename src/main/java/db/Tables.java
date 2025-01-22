cff06237494fc2db6d4c5734a5b94180aa25fdc2
ji-woong-song <thdwldnd7@naver.com>
2025-01-22 18:27:17 +0900

    fix: post테이블에 user_id의 데이터 형식이 Int -> Varchar(255)로 변경

--- src/main/java/db/Tables.java
+++ src/main/java/db/Tables.java
@@ -11,7 +11,7 @@ public class Tables {
                 "    id VARCHAR(255) PRIMARY KEY," +
                 "    username VARCHAR(255) NOT NULL," +
                 "    password VARCHAR(255) NOT NULL," +
-                "    email VARCHAR(255) NOT NULL UNIQUE" +
+                "    email VARCHAR(255) NOT NULL" +
                 ");";
         try {
             Statement stmt = connection.createStatement();
@@ -25,7 +25,7 @@ public class Tables {
         String sql = "" +
                 "CREATE TABLE IF NOT EXISTS posts (" +
                 "    id INT AUTO_INCREMENT PRIMARY KEY," +
-                "    user_id INT," +
+                "    user_id VARCHAR(255) NOT NULL," +
                 "    title VARCHAR(255) NOT NULL," +
                 "    body TEXT," +
                 "    FOREIGN KEY (user_id) REFERENCES users(id)" +
