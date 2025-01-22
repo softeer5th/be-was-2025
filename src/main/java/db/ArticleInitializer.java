154a5aaee92a36e71f587a695512f887eee4c639
khw7385 <khw7385@naver.com>
2025-01-23 01:52:57 +0900

    fix: 같은 트랜잭션을 공유하며 데이터 추가 시 동일한 created_at 갖는 문제 해결 트랜잭션을 분리

--- src/main/java/db/ArticleInitializer.java
+++ src/main/java/db/ArticleInitializer.java
@@ -10,9 +10,12 @@ public class ArticleInitializer {
     private final ArticleDao articleDao = ArticleDao.getInstance();
     private final TransactionTemplate transactionTemplate = TransactionTemplate.getInstance();
     public void init(){
-        transactionTemplate.executeWithoutResult(this::addArticle);
+        transactionTemplate.executeWithoutResult(this::addArticle, "article1");
+        transactionTemplate.executeWithoutResult(this::addArticle, "article2");
+        transactionTemplate.executeWithoutResult(this::addArticle, "article3");
+
     }
     private void addArticle(Transaction transaction, Object[] args){
-        articleDao.save(transaction, 1L, new Article(null, "ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ", new User(1L)));
+        articleDao.save(transaction, 1L, new Article(null, (String)args[0], new User(1L)));
     }
 }
