6912385416b59039ff2e2173a6adab383a76d9e1
khw7385 <khw7385@naver.com>
2025-01-22 10:16:33 +0900

    refactor: 에러 코드 객체 이름 변경 ALREADY_EXIST_USER_ID -> ALREADY_EXIST_LOGIN_ID

--- src/main/java/exception/ErrorCode.java
+++ src/main/java/exception/ErrorCode.java
@@ -1,7 +1,7 @@
 package exception;
 
 public enum ErrorCode {
-    ALREADY_EXIST_USER_ID("이미 존재하는 사용자 아이디입니다."),
+    ALREADY_EXIST_LOGIN_ID("이미 존재하는 사용자 아이디입니다."),
     ALREADY_EXIST_USER_NAME("이미 존재하는 사용자 이름입니다."),
     NOT_FOUND_USER_BY_USER_ID("사용자 아이디와 일치하는 사용자가 없습니다."),
     MISMATCH_PASSWORD("비밀번호가 일치하지 않습니다.");
