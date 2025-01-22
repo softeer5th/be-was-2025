bb38b9a795ab75b3f6503c35fbe283b98b7a9822
efdao <efdao@naver.com>
2025-01-22 15:35:42 +0900

    Feat: 비밀번호 변경 기능 동작 구현
    1. /mypage/index.html 수정
    
    2. UpdatePasswordHandler 추가
    
    3. ResponseBuilder 에 매핑

---
+++ src/main/java/handler/UpdatePasswordHandler.java
@@ -0,0 +1,27 @@
+package handler;
+
+import model.User;
+import util.UserManager;
+import util.enums.HttpStatusCode;
+import util.enums.Page;
+import webserver.request.Request;
+import webserver.response.Response;
+import webserver.session.Session;
+import webserver.session.SessionManager;
+
+public class UpdatePasswordHandler extends Handler {
+    @Override
+    public Response handle(Request request) {
+        Response response = new Response(request, HttpStatusCode.FOUND);
+        Session session = SessionManager.getSession(sessionId);
+        try {
+            User user = UserManager.updateUser(session.getUser(), request.getBody());
+            session.updateUser(user);
+            response.addHeader("Location", Page.MAIN_LOGIN.getPath());
+        } catch (IllegalArgumentException e) {
+            response.setStatusCode(HttpStatusCode.SEE_OTHER);
+            response.addHeader("location", Page.MY_PAGE.getPath());
+        }
+        return response;
+    }
+}
