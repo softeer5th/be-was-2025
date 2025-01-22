f557c3090ddaa92f4718d047102a8648405d69e4
kwon204 <dafeg3@naver.com>
2025-01-22 14:42:19 +0900

    feat: jdbc를 사용하는 기능 추가
    jdbc를 사용해서 h2와 연동

--- src/main/java/db/ArticleStore.java
+++ src/main/java/db/ArticleStore.java
@@ -1,25 +1,90 @@
 package db;
 
 import model.Article;
+import model.Comment;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.exception.UserNotFoundException;
 
-import java.util.Comparator;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class ArticleStore {
-    private static ConcurrentMap<String, Article> articles = new ConcurrentHashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(ArticleStore.class);
 
     public static void addArticle(Article article) {
-        articles.put(article.getArticleId(), article);
+        String sql = "insert into ARTICLE values(?, ?, ?)";
+        try (Connection conn = Database.getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, article.getArticleId());
+            pstmt.setString(2, article.getContent());
+            pstmt.setString(3, article.getUser().getUserId());
+
+            pstmt.executeUpdate();
+
+            pstmt.close();
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
     }
 
-    public static Optional<Article> findArticleById(String id) {
-        return Optional.ofNullable(articles.get(id));
+    public static Optional<Article> findArticleById(int articleId) {
+        String sql = "select id, content, user_id from ARTICLE where id=?";
+        try (Connection conn = Database.getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, articleId);
+            ResultSet rs = pstmt.executeQuery();
+            if(!rs.first()) {
+                return Optional.empty();
+            }
+            int id = rs.getInt("id");
+            String content = rs.getString("content");
+            String userId = rs.getString("user_id");
+
+            User user = UserStore.findUserById(userId).orElseThrow(() -> new UserNotFoundException("해당 사용자가 없습니다."));
+
+            List<Comment> comments = CommentStore.findAllByArticle(id);
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+            return Optional.of(new Article(id, content, user, comments));
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
+        return Optional.empty();
     }
 
     public static List<Article> findAll() {
-        return articles.values().stream().sorted(Comparator.comparing(Article::getArticleId).reversed()).toList();
+        List<Article> articles = new ArrayList<>();
+        String sql = "select id, content, user_id from ARTICLE ORDER BY id desc";
+        try (Connection conn = Database.getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String content = rs.getString("content");
+                String userId = rs.getString("user_id");
+
+                User user = UserStore.findUserById(userId).orElseThrow(() -> new UserNotFoundException("해당 사용자가 없습니다."));
+
+                List<Comment> comments = CommentStore.findAllByArticle(id);
+
+                Article article = new Article(id, content, user, comments);
+                articles.add(article);
+            }
+            rs.close();
+            pstmt.close();
+            conn.close();
+            return articles;
+        } catch (SQLException e){
+            logger.error(e.getMessage());
+        }
+        return articles;
     }
 }
--- src/main/java/db/UserStore.java
+++ src/main/java/db/UserStore.java
@@ -1,24 +1,82 @@
 package db;
 
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class UserStore {
-    private static ConcurrentMap<String, User> users = new ConcurrentHashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(UserStore.class);
 
     public static void addUser(User user) {
-        users.put(user.getUserId(), user);
+        String sql = "insert into USERS values(?, ?, ?, ?)";
+        try (Connection conn = Database.getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getPassword());
+            pstmt.setString(4, user.getEmail());
+
+            pstmt.executeUpdate();
+
+            pstmt.close();
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
     }
 
     public static Optional<User> findUserById(String userId) {
-        return Optional.ofNullable(users.get(userId));
+        String sql = "select id, name, password, email from USERS where id=?";
+        try (Connection conn = Database.getConnection()){
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userId);
+            ResultSet rs = pstmt.executeQuery();
+            if(!rs.first()) {
+                return Optional.empty();
+            }
+            String id = rs.getString("id");
+            String name = rs.getString("name");
+            String password = rs.getString("password");
+            String email = rs.getString("email");
+
+            rs.close();
+            pstmt.close();
+            conn.close();
+            return Optional.of(new User(id, name, password, email));
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+        }
+        return Optional.empty();
     }
 
-    public static Collection<User> findAll() {
-        return users.values();
+    public static List<User> findAll() {
+        List<User> users = new ArrayList<>();
+        String sql = "select id, name, password, email from USERS";
+        try (Connection conn = Database.getConnection()){
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                String id = rs.getString("id");
+                String name = rs.getString("name");
+                String password = rs.getString("password");
+                String email = rs.getString("email");
+                User user = new User(id, name, password, email);
+                users.add(user);
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+            return users;
+        } catch(SQLException e) {
+            logger.error(e.getMessage());
+        }
+        return users;
     }
 }
