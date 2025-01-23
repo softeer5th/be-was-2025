12b766f72b4e8cccd2ee066b099b65a62228a507
keemjoonsung <kjs990114@naver.com>
2025-01-23 11:14:40 +0900

    [feat: DuplicateServletMappingException.java 구현] 서블렛 매핑이 중복되었을떄 예외를 반환함

--- src/main/java/webserver/http/servlet/DuplicateServletMappingException.java
+++
@@ -1,7 +0,0 @@
-package webserver.http.servlet;
-
-public class DuplicateServletMappingException extends RuntimeException {
-    public DuplicateServletMappingException(String message) {
-        super(message);
-    }
-}
