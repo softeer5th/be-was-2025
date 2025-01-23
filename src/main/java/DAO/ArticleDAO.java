d14ff504aff14db420a08412830b5efb04bbfed9
sunohkim <sunoh1222@gmail.com>
2025-01-23 14:21:11 +0900

    feat: DAO를 통한 데이터베이스 연동 기능 구현 UserDAO, SessionDAO, ArticleDAO를 통해 사용자, 세션, 게시물 관련 데이터베이스에 접근해 작업을 수행하는 기능을 구현했다.

--- src/main/java/DAO/ArticleDAO.java
+++ src/main/java/DAO/ArticleDAO.java
@@ -1,77 +1,16 @@
 package DAO;
 
-import model.User;
+import model.Article;
 import webserver.HTTPExceptions;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Time;
 
-public class UserDAO {
-    public void addUser(User user) {
-        String sql = "INSERT INTO users (userID, password, name, email) VALUES (?,?,?,?)";
-        Connection connection = null;
-        try {
-            connection = H2Connection.getConnection();
-            connection.setAutoCommit(false);
-            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-                preparedStatement.setString(1, user.getUserId());
-                preparedStatement.setString(2, user.getPassword());
-                preparedStatement.setString(3, user.getName());
-                preparedStatement.setString(4, user.getEmail());
-                preparedStatement.executeUpdate();
-            }
-
-            connection.commit();
-        } catch (Exception e) {
-            if (connection != null) {
-                try {
-                    connection.rollback();
-                } catch (Exception rollbackException) {
-                    rollbackException.printStackTrace();
-                } finally {
-                    throw new HTTPExceptions.Error500("Failed to commit transaction.");
-                }
-            }
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.setAutoCommit(true);
-                    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-                    connection.close();
-                } catch (Exception closeException) {
-                    closeException.printStackTrace();
-                    throw new HTTPExceptions.Error500("Failed to close transaction.");
-                }
-            }
-        }
-    }
-
-    public User getUserById(String userId) {
-        String sql = "SELECT * FROM users WHERE userId = ?";
-        try {
-            Connection connection = H2Connection.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, userId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                return new User(
-                        resultSet.getString("userId"),
-                        resultSet.getString("password"),
-                        resultSet.getString("name")
-                );
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new HTTPExceptions.Error500("Failed to get user.");
-        }
-        return null;
-    }
-
-    public void deleteUserById(String userId) {
-        String sql = "DELETE FROM users WHERE userId = ?";
+public class ArticleDAO {
+    public void addArticle(Article article) {
+        String sql = "INSERT INTO articles (content, authorId, authorName, createTime) VALUES (? ,?, ?, ?)";
         Connection connection = null;
         try {
             connection = H2Connection.getConnection();
@@ -79,7 +18,10 @@ public class UserDAO {
             connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, userId);
+                preparedStatement.setString(1, article.getContent());
+                preparedStatement.setString(2, article.getAuthorId());
+                preparedStatement.setString(3, article.getAuthorName());
+                preparedStatement.setTime(4, Time.valueOf(article.getCreateTime()));
                 preparedStatement.executeUpdate();
             }
 
@@ -108,8 +50,76 @@ public class UserDAO {
         }
     }
 
-    public void deleteAllUsers() {
-        String sql = "DELETE FROM users";
+    public Article getLatestArticle() {
+        String sql = "SELECT * FROM articles ORDER BY id DESC LIMIT 1";
+        try {
+            Connection connection = H2Connection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Article(
+                        resultSet.getInt("id"),
+                        resultSet.getString("content"),
+                        resultSet.getString("authorId"),
+                        resultSet.getString("authorName"),
+                        resultSet.getTime("createTime").toLocalTime()
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new HTTPExceptions.Error500("Failed to get latest article");
+        }
+        return null;
+    }
+
+    public Article getNextArticle(int id) {
+        String sql = "SELECT * FROM articles WHERE id < (?) ORDER BY id DESC LIMIT 1";
+        try {
+            Connection connection = H2Connection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Article(
+                        resultSet.getInt("id"),
+                        resultSet.getString("content"),
+                        resultSet.getString("authorId"),
+                        resultSet.getString("authorName"),
+                        resultSet.getTime("createTime").toLocalTime()
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new HTTPExceptions.Error500("Failed to get next article");
+        }
+        return null;
+    }
+
+    public Article getPreviousArticle(int id) {
+        String sql = "SELECT * FROM articles WHERE id > (?) ORDER BY id ASC LIMIT 1";
+        try {
+            Connection connection = H2Connection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Article(
+                        resultSet.getInt("id"),
+                        resultSet.getString("content"),
+                        resultSet.getString("authorId"),
+                        resultSet.getString("authorName"),
+                        resultSet.getTime("createTime").toLocalTime()
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new HTTPExceptions.Error500("Failed to get previous article");
+        }
+        return null;
+    }
+
+    public void deleteAllArticles() {
+        String sql = "DELETE FROM articles";
         Connection connection = null;
         try {
             connection = H2Connection.getConnection();
--- src/main/java/DAO/SessionDAO.java
+++ src/main/java/DAO/SessionDAO.java
@@ -1,77 +1,17 @@
 package DAO;
 
-import model.User;
+import model.Session;
 import webserver.HTTPExceptions;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Time;
+import java.time.LocalTime;
 
-public class UserDAO {
-    public void addUser(User user) {
-        String sql = "INSERT INTO users (userID, password, name, email) VALUES (?,?,?,?)";
-        Connection connection = null;
-        try {
-            connection = H2Connection.getConnection();
-            connection.setAutoCommit(false);
-            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-                preparedStatement.setString(1, user.getUserId());
-                preparedStatement.setString(2, user.getPassword());
-                preparedStatement.setString(3, user.getName());
-                preparedStatement.setString(4, user.getEmail());
-                preparedStatement.executeUpdate();
-            }
-
-            connection.commit();
-        } catch (Exception e) {
-            if (connection != null) {
-                try {
-                    connection.rollback();
-                } catch (Exception rollbackException) {
-                    rollbackException.printStackTrace();
-                } finally {
-                    throw new HTTPExceptions.Error500("Failed to commit transaction.");
-                }
-            }
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.setAutoCommit(true);
-                    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-                    connection.close();
-                } catch (Exception closeException) {
-                    closeException.printStackTrace();
-                    throw new HTTPExceptions.Error500("Failed to close transaction.");
-                }
-            }
-        }
-    }
-
-    public User getUserById(String userId) {
-        String sql = "SELECT * FROM users WHERE userId = ?";
-        try {
-            Connection connection = H2Connection.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, userId);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                return new User(
-                        resultSet.getString("userId"),
-                        resultSet.getString("password"),
-                        resultSet.getString("name")
-                );
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new HTTPExceptions.Error500("Failed to get user.");
-        }
-        return null;
-    }
-
-    public void deleteUserById(String userId) {
-        String sql = "DELETE FROM users WHERE userId = ?";
+public class SessionDAO {
+    public void addSession(Session session) {
+        String sql = "INSERT INTO sessions (sessionId, userId, lastAccessTime, maxInactiveInterval) VALUES (?, ?, ?, ?)";
         Connection connection = null;
         try {
             connection = H2Connection.getConnection();
@@ -79,7 +19,10 @@ public class UserDAO {
             connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setString(1, userId);
+                preparedStatement.setString(1, session.getSessionId());
+                preparedStatement.setString(2, session.getUserId());
+                preparedStatement.setTime(3, Time.valueOf(session.getLastAccessTime()));
+                preparedStatement.setInt(4, session.getMaxInactiveInterval());
                 preparedStatement.executeUpdate();
             }
 
@@ -108,8 +51,124 @@ public class UserDAO {
         }
     }
 
-    public void deleteAllUsers() {
-        String sql = "DELETE FROM users";
+    public Session getSessionById(String sessionId) {
+        String sql = "SELECT * FROM sessions WHERE sessionId = ?";
+        try {
+            Connection connection = H2Connection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, sessionId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Session(
+                        resultSet.getString("sessionId"),
+                        resultSet.getString("userId"),
+                        resultSet.getTime("lastAccessTime").toLocalTime(),
+                        resultSet.getInt("maxInactiveInterval")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new HTTPExceptions.Error500("Failed to get session.");
+        }
+        return null;
+    }
+
+    public int getSessionMaxInactiveInterval(String sessionId) {
+        String sql = "SELECT * FROM sessions WHERE sessionId = ?";
+        try {
+            Connection connection = H2Connection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, sessionId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("maxInactiveInterval");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new HTTPExceptions.Error500("Failed to get session.");
+        }
+        return -1;
+    }
+
+    public void updateSessionLastAccessTime(String sessionId, LocalTime time) {
+        String sql = "UPDATE sessions SET lastAccessTime = ? WHERE sessionId = ?";
+        Connection connection = null;
+        try {
+            connection = H2Connection.getConnection();
+            connection.setAutoCommit(false);
+            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setTime(1, Time.valueOf(time));
+                preparedStatement.setString(2, sessionId);
+                preparedStatement.executeUpdate();
+            }
+
+            connection.commit();
+        } catch (Exception e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (Exception rollbackException) {
+                    rollbackException.printStackTrace();
+                } finally {
+                    throw new HTTPExceptions.Error500("Failed to commit transaction.");
+                }
+            }
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.setAutoCommit(true);
+                    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+                    connection.close();
+                } catch (Exception closeException) {
+                    closeException.printStackTrace();
+                    throw new HTTPExceptions.Error500("Failed to close transaction.");
+                }
+            }
+        }
+    }
+
+    public void deleteSession(String sessionId) {
+        String sql = "DELETE FROM sessions WHERE sessionId = ?";
+        Connection connection = null;
+        try {
+            connection = H2Connection.getConnection();
+            connection.setAutoCommit(false);
+            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, sessionId);
+                preparedStatement.executeUpdate();
+            }
+
+            connection.commit();
+        } catch (Exception e) {
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                } catch (Exception rollbackException) {
+                    rollbackException.printStackTrace();
+                } finally {
+                    throw new HTTPExceptions.Error500("Failed to commit transaction.");
+                }
+            }
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.setAutoCommit(true);
+                    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+                    connection.close();
+                } catch (Exception closeException) {
+                    closeException.printStackTrace();
+                    throw new HTTPExceptions.Error500("Failed to close transaction.");
+                }
+            }
+        }
+    }
+
+    public void deleteAllSessions() {
+        String sql = "DELETE FROM sessions";
         Connection connection = null;
         try {
             connection = H2Connection.getConnection();
