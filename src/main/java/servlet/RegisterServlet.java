8e78feb1db29c0b64ddacb4353bbec3aecbcdb51
keemjoonsung <kjs990114@naver.com>
2025-01-23 11:22:48 +0900

    [update: RegisterServlet.java 수정] 비밀번호, 패스워드, 이메일 형식 체크 후 각각의 예외페이지로 리다이렉션하도록 수정

--- src/main/java/servlet/RegisterServlet.java
+++ src/main/java/servlet/RegisterServlet.java
@@ -1,26 +1,47 @@
 package servlet;
 
-import db.Database;
+import db.h2.UserStorage;
 import model.User;
 import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
 import webserver.http.servlet.HttpServlet;
+import webserver.session.HttpSession;
 
 public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpRequest request, HttpResponse response) {
-        String userId = request.getParameter("userId");
-        String password = request.getParameter("password");
-        String name = request.getParameter("name");
-        String email = request.getParameter("email");
+        java.lang.String userId = request.getParameter("userId");
+        java.lang.String password = request.getParameter("password");
+        java.lang.String name = request.getParameter("name");
+        java.lang.String email = request.getParameter("email");
 
         User user = new User(userId, password, name, email);
-        if(Database.findUserById(userId) == null) {
-            Database.saveUser(user);
+
+        if(!User.validateUserId(user.getUserId())) {
+            response.sendRedirect("/registration/failed_invalid_id.html");
+            return;
+        }
+
+        if(!User.validateEmail(user.getEmail())) {
+            response.sendRedirect("/registration/failed_invalid_email.html");
+            return;
+        }
+
+        if(!User.validatePassword(user.getPassword())) {
+            response.sendRedirect("/registration/failed_invalid_password.html");
+            return;
+        }
+
+        UserStorage userStorage = UserStorage.getInstance();
+
+        if(userStorage.findUserById(userId) == null) {
+            HttpSession session = request.getSession();
+            user = userStorage.insert(user);
+            session.setAttribute("user", user);
             response.sendRedirect("/");
         } else {
-            response.sendRedirect("/registration/failed.html");
+            response.sendRedirect("/registration/failed_already_exists.html");
         }
     }
 }
