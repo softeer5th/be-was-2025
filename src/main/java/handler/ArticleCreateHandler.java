bf550e3819fb0cebdd5d15f33d95af382aefceae
uri010 <dbfl0461@gmail.com>
2025-01-23 14:41:50 +0900

    [Feat] 게시물 저장 핸들러 구현

--- src/main/java/handler/ArticleCreateHandler.java
+++ src/main/java/handler/ArticleCreateHandler.java
@@ -2,10 +2,17 @@ package handler;
 
 import db.ArticleDataManger;
 import db.SessionDataManager;
+import exception.BaseException;
+import exception.SessionErrorCode;
 import http.HttpRequestInfo;
 import http.HttpResponse;
+import http.HttpStatus;
+import model.Article;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 public class ArticleCreateHandler implements Handler {
     private static final Logger logger = LoggerFactory.getLogger(ArticleCreateHandler.class);
@@ -21,6 +28,23 @@ public class ArticleCreateHandler implements Handler {
     @Override
     public HttpResponse handle(HttpRequestInfo request) {
         logger.info("Article handler called");
-        return null;
+        String userId = sessionDataManager.findUserIdBySessionID(request.getSession());
+
+        if (userId == null) {
+            logger.error("User id is null");
+            throw new BaseException(SessionErrorCode.USER_NOT_FOUND_FOR_SESSION);
+        }
+
+        logger.debug("Article Request body : " + request.getBody());
+        String content = URLDecoder.decode(request.getBody().substring("article_content=".length()), StandardCharsets.UTF_8);
+        Article article = new Article(userId, content, null);
+        articleDataManger.addArticle(article);
+        logger.debug("Article added to DB");
+
+        HttpResponse response = new HttpResponse();
+        response.setStatus(HttpStatus.CREATED);
+
+        return response;
     }
+
 }
